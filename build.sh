e87cab4f0ec3d661aeb2befb086bac8b448cf596
pgoldenb <>
2010-02-19 22:24:11 +0000

    Replaced build.sh script with build.xml ant build script.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6.jar:lib/xercesImpl.jar
-
-javac -classpath $CLASSPATH -source 1.4 java/*.java
-mv java/*.class bin
-jar cf batch_cron.jar bin/*.class
