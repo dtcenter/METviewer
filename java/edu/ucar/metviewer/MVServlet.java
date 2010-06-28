b20c757e2190053c9a35656cb9595c7e3996be90
pgoldenb <>
2010-06-28 19:19:10 +0000

    Developed AJAX communication functionality between the servlet and browser page

--- java/edu/ucar/metviewer/MVServlet.java
+++ java/edu/ucar/metviewer/MVServlet.java
@@ -1,6 +1,7 @@
 package edu.ucar.metviewer;
 
 import java.io.*;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -10,15 +11,41 @@ public class MVServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
-        response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
+
+        //response.setContentType("text/html");
+        response.setContentType("text/plain");
+        
+        //  print out the HTTP GET params
+        Map params = request.getParameterMap();
+        ArrayList listParamsArr = new ArrayList();
+        listParamsArr.addAll( params.entrySet() );
+        Map.Entry[] listParams = (Map.Entry[])listParamsArr.toArray(new Map.Entry[]{});
+        out.println("params:");
+        for(int i=0; i < listParams.length; i++){
+        	Object objKey = listParams[i].getKey();
+        	Object objVal = listParams[i].getValue();
+        	String strVal = "(unknown)";
+        	if( objVal instanceof String[] ){ strVal = ((String[])objVal)[0]; }
+        	out.println("  " + objKey.toString() + ": " + strVal);
+        }
+        
         out.println("howdy from MVServlet");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
+    	
+    	PrintWriter out = response.getWriter();
+        response.setContentType("text/plain");
+
+    	BufferedReader reader = new BufferedReader( new InputStreamReader(request.getInputStream()) );
+    	String strLine = "";
+    	String strRequestBody = "";
+    	while( (strLine = reader.readLine()) != null ){ strRequestBody = strRequestBody + strLine; }
+    	out.println("post data:\n  " + strRequestBody);
+    	
         doGet(request, response);
     }
 
