12d04f81726029432777ad14738cc80764c2fd93
pgoldenb <>
2010-07-01 23:30:35 +0000

    Added configuration functionality and logging

--- java/edu/ucar/metviewer/MVServlet.java
+++ java/edu/ucar/metviewer/MVServlet.java
@@ -4,9 +4,26 @@ import java.io.*;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import org.apache.log4j.*;
 
 public class MVServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger _logger = Logger.getLogger("edu.ucar.metviewer.MVServlet");
+	
+	public void init() throws ServletException {
+		_logger.error("init() - loading properties...");
+		Properties prop = new Properties();
+		try{
+			prop.load(new FileInputStream("mvservlet.properties"));
+		}catch(Exception e){
+			_logger.error("init() - ERROR: caught " + e.getClass() + " loading properties: " + e.getMessage());
+		}		
+		_logger.error("init() - done loading properties: " + prop.getProperty("db.username"));
+	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
@@ -44,9 +61,78 @@ public class MVServlet extends HttpServlet {
     	String strLine = "";
     	String strRequestBody = "";
     	while( (strLine = reader.readLine()) != null ){ strRequestBody = strRequestBody + strLine; }
-    	out.println("post data:\n  " + strRequestBody);
+    	_logger.info("doPost():  " + strRequestBody);
+       
+        try{
+			//  instantiate and configure the xml parser
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			//dbf.setSchema(schema);
+		    dbf.setValidating(false);
+		    dbf.setNamespaceAware(false);
+		    
+			DocumentBuilder builder = dbf.newDocumentBuilder();
+			builder.setErrorHandler(new ErrorHandler(){
+				public void error(SAXParseException exception)		{ printException("error", exception);		}
+				public void fatalError(SAXParseException exception)	{ printException("fatalError", exception);	}	
+				public void warning(SAXParseException exception)	{ printException("warning", exception);		}
+				
+				public void printException(String type, SAXParseException e){
+					System.out.println("  **  ERROR: " + e.getMessage() + "\n" +
+									   "      line: " + e.getLineNumber() + "  column: " + e.getColumnNumber());					
+				}
+			});
+			
+			//  parse the input document and build the MVNode data structure
+			//Document doc = builder.parse(request.getInputStream());
+			Document doc = builder.parse( new ByteArrayInputStream(strRequestBody.getBytes()) );
+			MVNode nodeReq = new MVNode(doc.getFirstChild());
+			
+			for(int i=0; i < nodeReq._children.length; i++){
+				MVNode nodeCall = nodeReq._children[i];
+				if( nodeCall._tag.equalsIgnoreCase("list_db") ){
+					String[] listDB = listDB();
+				}
+			}
+			String strVal = nodeReq._value;
+        }catch(Exception e){
+        	out.println("ERROR: caught " + e.getClass() + ": " + e.getMessage());
+        }
     	
         doGet(request, response);
     }
+    
+    public static String[] listDB() throws Exception{
+    	
+    	String strCmd = "mysql -upgoldenb -ppgoldenb -s -e\"show databases;\"";
+    	String[] listCmd = {"mysql", "-upgoldenb", "-ppgoldenb", "-s", "-e\\\"show databases\\\""};
+    	_logger.debug("listDB() - running command: " + strCmd);
+    	
+    	//  issue the list databases command
+		Process proc = Runtime.getRuntime().exec(listCmd);
+		if( MVBatch._boolProcWait ){
+			proc.waitFor();
+		} else {
+			try{
+				Thread.sleep(10000);
+			}catch(InterruptedException ie){}
+		}
 
+		//  build a list of the METViewer databases
+		ArrayList listDB = new ArrayList();
+		BufferedReader readerProcIn = new BufferedReader( new InputStreamReader(proc.getInputStream()) );		
+		while( readerProcIn.ready() ){
+			String strDB = readerProcIn.readLine();
+			if( !strDB.startsWith("metvdb_") ){ listDB.add(strDB); } 
+		}
+		readerProcIn.close();			
+		
+		//  print the error information to the log file
+		String strErr = "";
+		BufferedReader readerProcError = new BufferedReader( new InputStreamReader(proc.getErrorStream()) );		
+		while( readerProcError.ready() ){ strErr += readerProcError.readLine() + "\n"; }
+		readerProcError.close();
+		if( !"".equals(strErr) ){ _logger.debug("listDB() - error information: " + strErr); }
+
+		return new String[]{};
+    }
 }
