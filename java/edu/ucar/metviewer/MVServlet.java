d6c0418b12d56af944da87692c7a13d3fb19b448
pgoldenb <>
2010-07-02 18:04:04 +0000

    Moved the mvservlet.properties file and added database listing and connection functionality to the web app

--- java/edu/ucar/metviewer/MVServlet.java
+++ java/edu/ucar/metviewer/MVServlet.java
@@ -1,6 +1,7 @@
 package edu.ucar.metviewer;
 
 import java.io.*;
+import java.sql.*;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,15 +15,23 @@ public class MVServlet extends HttpServlet {
 	
 	private static final Logger _logger = Logger.getLogger("edu.ucar.metviewer.MVServlet");
 	
+	public static String _strDBHost = "";
+	public static String _strDBUser = "";
+	public static String _strDBPassword = "";
+	public static String[] _listDB = {};
+	
 	public void init() throws ServletException {
-		_logger.error("init() - loading properties...");
-		Properties prop = new Properties();
+		_logger.debug("init() - loading properties...");
 		try{
-			prop.load(new FileInputStream("mvservlet.properties"));
+			ResourceBundle bundle = ResourceBundle.getBundle("mvservlet");
+			_strDBHost     = bundle.getString("db.host");
+			_strDBUser     = bundle.getString("db.user");
+			_strDBPassword = bundle.getString("db.password");
+			_listDB        = bundle.getString("db.list").split("\\s*,\\s*");
 		}catch(Exception e){
 			_logger.error("init() - ERROR: caught " + e.getClass() + " loading properties: " + e.getMessage());
-		}		
-		_logger.error("init() - done loading properties: " + prop.getProperty("db.username"));
+		}				
+		_logger.debug("init() - done loading properties");
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -53,16 +62,20 @@ public class MVServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
-    	
-    	PrintWriter out = response.getWriter();
-        response.setContentType("text/plain");
-
+        //  read the body of the request
     	BufferedReader reader = new BufferedReader( new InputStreamReader(request.getInputStream()) );
     	String strLine = "";
     	String strRequestBody = "";
     	while( (strLine = reader.readLine()) != null ){ strRequestBody = strRequestBody + strLine; }
-    	_logger.info("doPost():  " + strRequestBody);
+    	_logger.debug("doPost() - request: " + strRequestBody);
        
+    	//  initialize the response writer 
+    	PrintWriter out = response.getWriter();
+        response.setContentType("text/plain");
+        
+        HttpSession session = request.getSession();
+
+        //  attempt to parse the body of the request
         try{
 			//  instantiate and configure the xml parser
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -77,8 +90,7 @@ public class MVServlet extends HttpServlet {
 				public void warning(SAXParseException exception)	{ printException("warning", exception);		}
 				
 				public void printException(String type, SAXParseException e){
-					System.out.println("  **  ERROR: " + e.getMessage() + "\n" +
-									   "      line: " + e.getLineNumber() + "  column: " + e.getColumnNumber());					
+					_logger.error("doPost() - xml parser error: " + e.getMessage() + "\n      line: " + e.getLineNumber() + "  column: " + e.getColumnNumber());					
 				}
 			});
 			
@@ -86,53 +98,54 @@ public class MVServlet extends HttpServlet {
 			//Document doc = builder.parse(request.getInputStream());
 			Document doc = builder.parse( new ByteArrayInputStream(strRequestBody.getBytes()) );
 			MVNode nodeReq = new MVNode(doc.getFirstChild());
+			String strResp = "";
 			
 			for(int i=0; i < nodeReq._children.length; i++){
 				MVNode nodeCall = nodeReq._children[i];
+
+				//  <list_db> request
 				if( nodeCall._tag.equalsIgnoreCase("list_db") ){
-					String[] listDB = listDB();
+					strResp = "<list_db>";
+					for(int j=0; j < _listDB.length; j++){ strResp += "<db>" + _listDB[j] + "</db>"; }
+					strResp += "</list_db>";
+			    	_logger.debug("doPost() - list_db response: " + strRequestBody);
+				}
+				
+				//  <connect_db> request
+				else if( nodeCall._tag.equalsIgnoreCase("connect_db") ){
+					strResp = "<connect_db>";
+					String strDB = nodeCall._value;
+			    	_logger.debug("doPost() - connect_db: connecting to " + strDB);
+					
+					//  if an open connection exists in the session, close it
+					Connection con = null;
+					if( null != session.getAttribute("con") ){ con = (Connection)session.getAttribute("con"); }
+					if( null != con && !con.isClosed() ){
+				    	_logger.debug("doPost() - connect_db: disconnecting existing connection");
+				    	strResp += "<disconnect>" + con.getMetaData().getURL() + "</disconnect>";
+				    	con.close();
+					}
+					
+					//  attempt to open a new connection and store it in the session
+					try {
+						Class.forName("com.mysql.jdbc.Driver").newInstance();
+						con = DriverManager.getConnection("jdbc:mysql://" + _strDBHost + "/" + strDB, _strDBUser, _strDBPassword);
+						if( con.isClosed() )	throw new Exception("METViewer error: database connection failed");
+						session.setAttribute("con", con);
+						strResp += "<connected>" + con.getMetaData().getURL() + "</connected>";
+				    	_logger.debug("doPost() - connect_db: connected to " + strDB);
+					} catch(Exception ex){
+						strResp += "<error>caught " + ex.getClass() + ": " + ex.getMessage() + "</error>";
+				    	_logger.error("doPost() - connect_db: caught " + ex.getClass() + " connecting to database: " + ex.getMessage());
+					}
+					
+					strResp += "</connect_db>";
 				}
 			}
-			String strVal = nodeReq._value;
+			
+			out.println(strResp);
         }catch(Exception e){
-        	out.println("ERROR: caught " + e.getClass() + ": " + e.getMessage());
+        	_logger.error("doPost() - caught " + e.getClass() + ": " + e.getMessage());
         }
-    	
-        doGet(request, response);
-    }
-    
-    public static String[] listDB() throws Exception{
-    	
-    	String strCmd = "mysql -upgoldenb -ppgoldenb -s -e\"show databases;\"";
-    	String[] listCmd = {"mysql", "-upgoldenb", "-ppgoldenb", "-s", "-e\\\"show databases\\\""};
-    	_logger.debug("listDB() - running command: " + strCmd);
-    	
-    	//  issue the list databases command
-		Process proc = Runtime.getRuntime().exec(listCmd);
-		if( MVBatch._boolProcWait ){
-			proc.waitFor();
-		} else {
-			try{
-				Thread.sleep(10000);
-			}catch(InterruptedException ie){}
-		}
-
-		//  build a list of the METViewer databases
-		ArrayList listDB = new ArrayList();
-		BufferedReader readerProcIn = new BufferedReader( new InputStreamReader(proc.getInputStream()) );		
-		while( readerProcIn.ready() ){
-			String strDB = readerProcIn.readLine();
-			if( !strDB.startsWith("metvdb_") ){ listDB.add(strDB); } 
-		}
-		readerProcIn.close();			
-		
-		//  print the error information to the log file
-		String strErr = "";
-		BufferedReader readerProcError = new BufferedReader( new InputStreamReader(proc.getErrorStream()) );		
-		while( readerProcError.ready() ){ strErr += readerProcError.readLine() + "\n"; }
-		readerProcError.close();
-		if( !"".equals(strErr) ){ _logger.debug("listDB() - error information: " + strErr); }
-
-		return new String[]{};
     }
 }
