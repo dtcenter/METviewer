a65446ff8370bc5ffece47a669e1a5207eb846f4
pgoldenb <>
2010-07-09 21:54:23 +0000

    Added webapp capability to logically AND fixed value conditions

--- java/edu/ucar/metviewer/MVServlet.java
+++ java/edu/ucar/metviewer/MVServlet.java
@@ -69,10 +69,9 @@ public class MVServlet extends HttpServlet {
     	while( (strLine = reader.readLine()) != null ){ strRequestBody = strRequestBody + strLine; }
     	_logger.debug("doPost() - request: " + strRequestBody);
        
-    	//  initialize the response writer 
+    	//  initialize the response writer and session
     	PrintWriter out = response.getWriter();
-        response.setContentType("text/plain");
-        
+        response.setContentType("text/plain");        
         HttpSession session = request.getSession();
 
         //  attempt to parse the body of the request
@@ -108,82 +107,10 @@ public class MVServlet extends HttpServlet {
 					strResp = "<list_db>";
 					for(int j=0; j < _listDB.length; j++){ strResp += "<db>" + _listDB[j] + "</db>"; }
 					strResp += "</list_db>";
-			    	//_logger.debug("doPost() - list_db response: " + strRequestBody);
 				}
 				
-				//  <connect_db> request
-				else if( nodeCall._tag.equalsIgnoreCase("connect_db") ){
-
-					//  parse the input request, and initialize the response
-					strResp = "<connect_db>";
-					String strDB = nodeCall._value;
-			    	_logger.debug("doPost() - connect_db: connecting to " + strDB);
-					
-					//  if an open connection exists in the session, close it
-					Connection con = null;
-					if( null != session.getAttribute("con") ){ con = (Connection)session.getAttribute("con"); }
-					if( null != con && !con.isClosed() ){
-				    	_logger.debug("doPost() - connect_db: disconnecting existing connection");
-				    	strResp += "<disconnect>" + con.getMetaData().getURL() + "</disconnect>";
-				    	con.close();
-					}
-					
-					//  attempt to open a new connection and store it in the session
-					try {
-						Class.forName("com.mysql.jdbc.Driver").newInstance();
-						con = DriverManager.getConnection("jdbc:mysql://" + _strDBHost + "/" + strDB, _strDBUser, _strDBPassword);
-						if( con.isClosed() )	throw new Exception("METViewer error: database connection failed");
-						session.setAttribute("con", con);
-						strResp += "<connected>" + con.getMetaData().getURL() + "</connected>";
-				    	_logger.debug("doPost() - connect_db: connected to " + strDB);
-					} catch(Exception ex){
-						strResp += "<error>caught " + ex.getClass() + ": " + ex.getMessage() + "</error>";
-				    	_logger.error("doPost() - connect_db: caught " + ex.getClass() + " connecting to database: " + ex.getMessage());
-					}
-					
-					strResp += "</connect_db>";
-				}
-				
-				//  <list_val> request
-				else if( nodeCall._tag.equalsIgnoreCase("list_val") ){
-					
-					//  parse the input request, and initialize the response
-					strResp = "<list_val>";
-					String strId = nodeCall._children[0]._value;
-					String strField = nodeCall._children[1]._value;
-			    	_logger.debug("doPost() - list_val: listing values for field " + strField + " and id " + strId);
-			    	strResp += "<id>" + strId + "</id>";
-					
-					//  validate the database connection
-					Connection con = null;
-					if( null == session.getAttribute("con") ){						
-				    	_logger.error("doPost() - list_val: no session connection found");
-				    	strResp += "<error>no session connection found</error>";
-					} 
-					
-					//  if the connection is valid, query the database for field values
-					else {
-						
-						//  build a select statment and execute it
-						con = (Connection)session.getAttribute("con");
-						String strFieldDB = strField.toLowerCase();
-						Statement stmt = con.createStatement();
-						String strSQL = "SELECT DISTINCT " + strFieldDB + " FROM stat_header ORDER BY " + strFieldDB;
-						stmt.executeQuery(strSQL);
-						_logger.debug("doPost() - list_val: " + strSQL);
-						
-						//  add the list of field values from the query to the response
-						int intNumVal = 0;
-						ResultSet res = stmt.getResultSet();
-						while( res.next() ){
-							strResp += "<val>" + res.getString(1) + "</val>";
-							intNumVal++;
-						}
-						_logger.debug("doPost() - list_val: returned " + intNumVal + " values");
-						stmt.close();
-					}
-					strResp += "</list_val>";
-				}
+				else if( nodeCall._tag.equalsIgnoreCase("connect_db") ){ strResp += handleConnectDB(nodeCall, session); }
+				else if( nodeCall._tag.equalsIgnoreCase("list_val") )  { strResp += handleListVal  (nodeCall, session); }
 				
 				//  not handled
 				else {
@@ -201,5 +128,103 @@ public class MVServlet extends HttpServlet {
         	_logger.error("doPost() - caught " + e.getClass() + ": " + e.getMessage());
         	out.println("<error>caught " + e.getClass() + ": " + e.getMessage() + "</error>");
         }
+    }
+    
+    /**
+     * Attempt to connect to the specified database, disconnecting any existing connection, and return
+     * information about the connection
+     * @param nodeCall MVNode containing request information
+     * @param session HttpSession accompanying the request
+     * @return XML response information
+     * @throws Exception
+     */
+    public static String handleConnectDB(MVNode nodeCall, HttpSession session) throws Exception{
+    	
+		//  parse the input request, and initialize the response
+		String strResp = "<connect_db>";
+		String strDB = nodeCall._value;
+    	_logger.debug("handleConnectDB() - connect_db: connecting to " + strDB);
+		
+		//  if an open connection exists in the session, close it
+		Connection con = null;
+		if( null != session.getAttribute("con") ){ con = (Connection)session.getAttribute("con"); }
+		if( null != con && !con.isClosed() ){
+	    	_logger.debug("handleConnectDB() - connect_db: disconnecting existing connection");
+	    	strResp += "<disconnect>" + con.getMetaData().getURL() + "</disconnect>";
+	    	con.close();
+		}
+		
+		//  attempt to open a new connection and store it in the session
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://" + _strDBHost + "/" + strDB, _strDBUser, _strDBPassword);
+			if( con.isClosed() )	throw new Exception("METViewer error: database connection failed");
+			session.setAttribute("con", con);
+			strResp += "<connected>" + con.getMetaData().getURL() + "</connected>";
+	    	_logger.debug("handleConnectDB() - connect_db: connected to " + strDB);
+		} catch(Exception ex){
+			strResp += "<error>caught " + ex.getClass() + ": " + ex.getMessage() + "</error>";
+	    	_logger.error("handleConnectDB() - connect_db: caught " + ex.getClass() + " connecting to database: " + ex.getMessage());
+		}
+		
+		strResp += "</connect_db>";
+		return strResp;
+    }
+    
+    /**
+     * Attempt to list database values for the specified field with the specified constraints
+     * @param nodeCall MVNode containing request information
+     * @param session HttpSession accompanying the request
+     * @return XML response information
+     * @throws Exception
+     */
+    public static String handleListVal(MVNode nodeCall, HttpSession session) throws Exception{
+
+    	//  validate the database connection
+		if( null == session.getAttribute("con") ){						
+	    	_logger.error("handleListVal() - list_val: no session connection found");
+	    	return "<error>no session connection found</error>";
+		} 
+		Connection con = (Connection)session.getAttribute("con");
+				
+		//  parse the input request, and initialize the response
+		String strResp = "<list_val>";
+		String strId = nodeCall._children[0]._value;
+		String strFixField = nodeCall._children[1]._value;
+    	_logger.debug("handleListVal() - list_val: listing values for field " + strFixField + " and id " + strId);
+    	strResp += "<id>" + strId + "</id>";
+    	
+    	//  parse the list of constraints into a SQL where clause
+    	String strWhere = "";
+    	for(int i=2; i < nodeCall._children.length; i++){
+    		MVNode nodeField = nodeCall._children[i];
+    		strWhere += (2 < i? "AND " : "WHERE ") + nodeField._name.toLowerCase() + " IN (";
+    		for(int j=0; j < nodeField._children.length; j++){
+    			strWhere += (0 < j? ", " : "") + "'" + nodeField._children[j]._value + "'";
+    		}
+    		strWhere += ") ";
+    	}
+		
+		//  build a select statment and execute it
+		String strFieldDB = strFixField.toLowerCase();
+		Statement stmt = con.createStatement();
+		String strSQL = "SELECT DISTINCT " + strFieldDB + " FROM stat_header " + strWhere + "ORDER BY " + strFieldDB;
+		_logger.debug("handleListVal() - list_val: " + strSQL);
+		long intStart = (new java.util.Date()).getTime();
+		stmt.executeQuery(strSQL);
+		
+		//  add the list of field values from the query to the response
+		int intNumVal = 0;
+		ResultSet res = stmt.getResultSet();
+		while( res.next() ){
+			strResp += "<val>" + res.getString(1) + "</val>";
+			intNumVal++;
+		}
+		_logger.debug("handleListVal() - list_val: returned " + intNumVal + " values in " + MVUtil.formatTimeSpan((new java.util.Date()).getTime() - intStart));
+		
+		//  clean up  
+		stmt.close();
+		strResp += "</list_val>";
+		return strResp;
     }
 }
