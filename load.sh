5d858ca79a8a2cc68b4a17ffa116614632f68b55
pgoldenb <>
2011-02-10 18:08:22 +0000

    Added code to generalize the run location

--- load.sh
+++ load.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# construct the classpath for MVLoad
-CLASSPATH=lib/log4j-1.2.15.jar
-CLASSPATH=$CLASSPATH:lib/mysql-connector-java-5.1.6.jar
-CLASSPATH=$CLASSPATH:lib/xercesImpl.jar
-CLASSPATH=$CLASSPATH:bin
+JAVA=/d1/pgoldenb/usr/local/bin/java
+MV_HOME=/home/pgoldenb/apps/verif/metviewer
 
-java -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.MVLoad $@
+# construct the classpath for MVLoad
+CLASSPATH=$MV_HOME/lib/log4j-1.2.15.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/mysql-connector-java-5.1.6.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/bin
+
+$JAVA -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.MVLoad $@
