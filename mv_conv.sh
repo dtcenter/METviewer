8cd611e45ba9e794a11ca600cd9ad5c17daabd67
pgoldenb <>
2011-02-11 22:28:05 +0000

    Moved scripts into bin folder

--- mv_conv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# construct the classpath for MVLoad
-CLASSPATH=lib/log4j-1.2.15.jar
-CLASSPATH=$CLASSPATH:lib/mysql-connector-java-5.1.6.jar
-CLASSPATH=$CLASSPATH:lib/xercesImpl.jar
-CLASSPATH=$CLASSPATH:bin
-
-java -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.MVConv $@
