5c7906084e819d1fab03452a68e590eaaaea522b
pgoldenb <>
2011-03-17 20:06:29 +0000

    Fixed some inconsitencies and outdated references

--- bin/mv_batch.sh
+++ bin/mv_batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 JAVA=/d1/pgoldenb/usr/local/bin/java
-METVIEWER=/home/pgoldenb/apps/verif/metviewer
+MV_HOME=/home/pgoldenb/apps/verif/metviewer
 
 # construct the classpath for MVBatch
-CLASSPATH=$METVIEWER/lib/log4j-1.2.15.jar
-CLASSPATH=$CLASSPATH:$METVIEWER/lib/mysql-connector-java-5.1.6.jar
-CLASSPATH=$CLASSPATH:$METVIEWER/lib/xercesImpl.jar
-CLASSPATH=$CLASSPATH:$METVIEWER/bin
+CLASSPATH=$MV_HOME/lib/log4j-1.2.15.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/mysql-connector-java-5.1.6.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/build
 
 $JAVA -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.MVBatch $@
