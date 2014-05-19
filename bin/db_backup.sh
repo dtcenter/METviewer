584e6553b48afd0f23cdce531b4e5594dd853d06
tatiana <>
2014-05-19 14:20:51 +0000

    improvements

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -24,47 +24,53 @@ SUCCESSMESSAGE="/d3/projects/METViewer/src/apps/verif/metviewer/bin/successMessa
 MAIN_START=$(date +%s.%N)
 mysql -umvuser -pmvuser -e 'show databases;' | {
 	while read db_name ; do
-		if [ "$db_name" != "information_schema" -a  "$db_name" != "Database"  -a  "$db_name" != "mysql" ];
+		if [ "$db_name" != "information_schema"  -a "$db_name" != "performance_schema" -a "$db_name" != "Database"  -a  "$db_name" != "mysql" ];
 		then
-		  printf '\n\nAbout to back up database: '$db_name
+		  printf '\n\nAbout to back up database: '$db_name'\n'
+		  LAST_DATE_DEV_STR="";
 			LAST_DATE_DEV_STR=$(mysql -umvuser -pmvuser -h dakota $db_name -e 'select max(load_date) from data_file ;' | sed -n '2p')
-      printf '\nLast date from dev: '$LAST_DATE_DEV_STR
-      LAST_DATE_PROD_STR=$(mysql -umvuser -pmvuser  $db_name -e 'select max(load_date) from data_file ;' | sed -n '2p')
-      if [ "$LAST_DATE_PROD_STR" != "NULL" ];
-      then
-        #database is real and has data - check if it needs to be based up
-        printf '\nLast date from prod: '$LAST_DATE_PROD_STR
-
-        LAST_DATE_DEV=$(date -d "$LAST_DATE_DEV_STR" +%s)
-        LAST_DATE_PROD=$(date -d "$LAST_DATE_PROD_STR" +%s)
-
-        #compate dates of uploading
-        if  [[ $LAST_DATE_DEV = "NULL" ||  $LAST_DATE_DEV -ne $LAST_DATE_PROD ]]  ;
+			if [[ $LAST_DATE_DEV_STR == "" ]];
+			then
+			  #there is no  data_file table in database -> new database - send email
+			  printf 'There is no  data_file table in database '$db_name' or the table is empty\n'
+        /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $ERRORMESSAGE
+      else
+        printf '\nLast date from dev: '$LAST_DATE_DEV_STR
+        LAST_DATE_PROD_STR=$(mysql -umvuser -pmvuser  $db_name -e 'select max(load_date) from data_file ;' | sed -n '2p')
+        if [ "$LAST_DATE_PROD_STR" != "NULL" ];
         then
-          #dates are different - changes were made - backup database
+          #database is real and has data - check if it needs to be based up
+          printf '\nLast date from prod: '$LAST_DATE_PROD_STR
 
-          #analyze it first
-          mysqlcheck -a -umvuser -pmvuser $db_name
-
-          mysqldump -umvuser -pmvuser $db_name > $db_name.sql
-          mysql -umvuser -pmvuser -hdakota  $db_name <  $db_name.sql
-          rm $db_name.sql
-          if [ $? -gt 0 ];
+          #compate dates of uploading
+          if  [ "$LAST_DATE_DEV_STR" != "$LAST_DATE_PROD_STR" ];
           then
-            #error detected during the backup - send email
-            /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $ERRORMESSAGE
+            #dates are different - changes were made - backup database
+
+            #analyze it first
+            mysqlcheck -a -umvuser -pmvuser $db_name
+
+            mysqldump -umvuser -pmvuser $db_name > $db_name.sql
+            mysql -umvuser -pmvuser -hdakota  $db_name <  $db_name.sql
+
+            if [ $? -gt 0 ];
+            then
+              #error detected during the backup - send email
+              /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $ERRORMESSAGE
+            else
+              #successful backup
+              printf '\n'$db_name' was backed up'
+              /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $SUCCESSMESSAGE
+            fi
+            rm $db_name.sql
           else
-            #successful backup
-            printf '\n'$db_name' was backed up'
-            /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $SUCCESSMESSAGE
+            #dates are the same - no changes were made - backup is not needed
+            printf '\n'$db_name' was not changed and was not backed up '
           fi
         else
-          #dates are the same - no changes were made - backup is not needed
-          printf '\n'$db_name' was not changed and was not backed up'
+          #database is empty - no backup
+          printf '\n'$db_name' does not have data. No backup '
         fi
-      else
-        #database is empty - no backup
-        printf '\n'$db_name' does not have data No backup'
       fi
     fi
 	done
@@ -73,7 +79,7 @@ mysql -umvuser -pmvuser -e 'show databases;' | {
 
 MAIN_END=$(date +%s.%N)
 MAIN_DIFF=$(echo "$MAIN_END - $MAIN_START" | bc)
-printf '\n\nBackup took : '$MAIN_DIFF'seconds'
+printf '\n\nBackup took : '$MAIN_DIFF' seconds''\n'
 
 #Run backup script at 3 AM  everyday
-# 00 9 * * * /d3/projects/METViewer/src/apps/verif/metviewer/bin/db_backup.sh > /d3/projects/METViewer/src/apps/verif/metviewer/bin/db_backup.log 2>&1
+# 0 9 * * * /d3/projects/METViewer/src/apps/verif/metviewer/bin/db_backup.sh > /d3/projects/METViewer/src/apps/verif/metviewer/bin/db_backup_$(date +"%Y-%m-%d").log 2>&1
