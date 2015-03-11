298536be78afe2d75197fdd1f0716f45297fb1de
tatiana <>
2015-03-11 19:40:14 +0000

    Add all stats from VSDB files

--- sql/mv_mysql.sql
+++ sql/mv_mysql.sql
@@ -364,6 +364,12 @@ CREATE TABLE line_data_cnt
     mad                 DOUBLE DEFAULT -9999,
     mad_bcl             DOUBLE DEFAULT -9999,
     mad_bcu             DOUBLE DEFAULT -9999,
+    pac                 DOUBLE DEFAULT -9999,
+    pac_ncl             DOUBLE DEFAULT -9999,
+    pac_ncu             DOUBLE DEFAULT -9999,
+    pac_bcl             DOUBLE DEFAULT -9999,
+    pac_bcu             DOUBLE DEFAULT -9999,
+
     
     CONSTRAINT line_data_cnt_data_file_id_pk
             FOREIGN KEY(data_file_id)
@@ -547,7 +553,22 @@ CREATE TABLE line_data_pstd
     brier               DOUBLE,
     brier_ncl           DOUBLE,
     brier_ncu           DOUBLE,
-    
+    briercl            DOUBLE DEFAULT -9999,
+    briercl_ncl        DOUBLE DEFAULT -9999,
+    briercl_ncu        DOUBLE DEFAULT -9999,
+    bss                 DOUBLE DEFAULT -9999,
+    bss_ncl             DOUBLE DEFAULT -9999,
+    bss_ncu             DOUBLE DEFAULT -9999,
+    inf                 DOUBLE DEFAULT -9999,
+    inf_ncl             DOUBLE DEFAULT -9999,
+    inf_ncu             DOUBLE DEFAULT -9999,
+    brier10               DOUBLE DEFAULT -9999,
+    brier10_ncl           DOUBLE DEFAULT -9999,
+    brier10_ncu           DOUBLE DEFAULT -9999,
+    brier90               DOUBLE DEFAULT -9999,
+    brier90_ncl           DOUBLE DEFAULT -9999,
+    brier90_ncu           DOUBLE DEFAULT -9999,
+
     PRIMARY KEY (line_data_id),
     CONSTRAINT line_data_pstd_data_file_id_pk
             FOREIGN KEY(data_file_id)
@@ -1061,6 +1082,67 @@ CREATE TABLE line_data_nbrcnt
             FOREIGN KEY(stat_header_id)
             REFERENCES stat_header(stat_header_id)
 );
+
+DROP TABLE IF EXISTS line_data_enscnt;
+CREATE TABLE line_data_enscnt
+(
+  stat_header_id INT UNSIGNED NOT NULL,
+   data_file_id INT UNSIGNED NOT NULL,
+   line_num INT UNSIGNED,
+   fcst_lead INT UNSIGNED,
+   fcst_valid_beg DATETIME,
+   fcst_valid_end DATETIME,
+   fcst_init_beg DATETIME,
+   obs_lead INT UNSIGNED,
+   obs_valid_beg DATETIME,
+   obs_valid_end DATETIME,
+
+   rpsf DOUBLE DEFAULT -9999,
+   rpsf_ncl DOUBLE DEFAULT -9999,
+   rpsf_ncu DOUBLE DEFAULT -9999,
+   rpsf_bcl DOUBLE DEFAULT -9999,
+   rpsf_bcu DOUBLE DEFAULT -9999,
+
+   rpscl DOUBLE DEFAULT -9999,
+   rpscl_ncl DOUBLE DEFAULT -9999,
+   rpscl_ncu DOUBLE DEFAULT -9999,
+   rpscl_bcl DOUBLE DEFAULT -9999,
+   rpscl_bcu DOUBLE DEFAULT -9999,
+
+   rpss DOUBLE DEFAULT -9999,
+   rpss_ncl DOUBLE DEFAULT -9999,
+   rpss_ncu DOUBLE DEFAULT -9999,
+   rpss_bcl DOUBLE DEFAULT -9999,
+   rpss_bcu DOUBLE DEFAULT -9999,
+
+   crpsf DOUBLE DEFAULT -9999,
+   crpsf_ncl DOUBLE DEFAULT -9999,
+   crpsf_ncu DOUBLE DEFAULT -9999,
+   crpsf_bcl DOUBLE DEFAULT -9999,
+   crpsf_bcu DOUBLE DEFAULT -9999,
+
+   crpscl DOUBLE DEFAULT -9999,
+   crpscl_ncl DOUBLE DEFAULT -9999,
+   crpscl_ncu DOUBLE DEFAULT -9999,
+   crpscl_bcl DOUBLE DEFAULT -9999,
+   crpscl_bcu DOUBLE DEFAULT -9999,
+
+   crpss DOUBLE DEFAULT -9999,
+   crpss_ncl DOUBLE DEFAULT -9999,
+   crpss_ncu DOUBLE DEFAULT -9999,
+   crpss_bcl DOUBLE DEFAULT -9999,
+   crpss_bcu DOUBLE DEFAULT -9999,
+
+
+
+   CONSTRAINT line_data_enscnt_file_id_pk
+   FOREIGN KEY (data_file_id)
+   REFERENCES data_file (data_file_id),
+   CONSTRAINT line_data_enscnt_stat_header_id_pk
+   FOREIGN KEY (stat_header_id)
+   REFERENCES stat_header (stat_header_id)
+);
+
 
 
 --  contains stat data for a particular stat_header record, which it points 
