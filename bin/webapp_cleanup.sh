e9594ebf37773a2b57a738af6f1f16f786246c7f
tatiana <>
2015-07-23 19:12:41 +0000

    new location for images/data files

--- bin/webapp_cleanup.sh
+++ bin/webapp_cleanup.sh
@@ -2,14 +2,14 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
-# This utility removes all output data older that 2 days that METViewer web app produces
+# This utility removes all output data older that 7 days that METViewer web app produces
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-find /opt/tomcat/webapps/metviewer/plots -mtime +2 -exec rm {} \;
-find /opt/tomcat/webapps/metviewer/xml -mtime +2 -exec rm {} \;
-find /opt/tomcat/webapps/metviewer/R_work/data -mtime +2 -exec rm {} \;
-find /opt/tomcat/webapps/metviewer/R_work/scripts -mtime +2 -exec rm {} \;
+find /var/www/dtcenter/met/metviewer_outputplots -mtime +7 -exec rm {} \;
+find /var/www/dtcenter/met/metviewer_output/xml -mtime +7 -exec rm {} \;
+find /var/www/dtcenter/met/metviewer_output/data -mtime +7 -exec rm {} \;
+find /var/www/dtcenter/met/metviewer_output/scripts -mtime +7 -exec rm {} \;
 
 #Run backup script at 0 AM  everyday
 # 0 0 * * * /d3/projects/METViewer/src_dev/apps/verif/metviewer/bin/webapp_cleanup.sh
