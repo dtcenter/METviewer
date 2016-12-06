d56595b235149803dabcee20f1e7297374150c01
TatianaBurek <tatiana@ucar.edu>
2016-12-06 12:10:06 -0700

    initial version

---
+++ bin/mv_prune.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+JAVA=/usr/local/jdk/bin/java
+MV_HOME=/d3/projects/METViewer/src/apps/METViewer
+
+# construct the classpath for MVPruneDB
+CLASSPATH=$MV_HOME/lib/log4j-1.2.15.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/mysql-connector-java-5.1.6.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xml-apis.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/bonecp-0.7.1.RELEASE.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-api-1.7.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-log4j12-1.7.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/guava-14.0.1.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/dist/lib/metviewer.jar
+
+$JAVA -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.prune.MVPruneDB $@
