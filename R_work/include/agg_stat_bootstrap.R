2396f73f1ca855661edbaef4c6a3a62c10bc9361
TatianaBurek <tatiana@ucar.edu>
2017-01-31 12:15:49 -0700

    updated EE

--- R_work/include/agg_stat_bootstrap.R
+++ R_work/include/agg_stat_bootstrap.R
@@ -17,8 +17,45 @@ dfStatsRec = read.delim(strInputDataFile);
 dfPlot1 = data.frame();
 
 if(boolEventEqual){
-  dfStatsRec = equalizeAggStatsAgainstValues(strInputEeDataFile,listFixedValEx,listDep1Plot,listDep2Plot,listSeries1Val,listSeries2Val,dfStatsRec);
+  eeStats = read.delim(strInputEeDataFile);
+    dfPlot1 = data.frame();
 
+    for( strDep1Name in names(listDep1Plot) ){
+      for(strSeriesVal in names(listSeries1Val)){
+        vectValPerms = c();
+        for(index in 1:length(listSeries1Val[[strSeriesVal]])){
+          vectValPerms= append(vectValPerms, strsplit(listSeries1Val[[strSeriesVal]][index], ",")[[1]]);
+        }
+        fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep1Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
+        eeStatsEqualize = eeStats[eeStats$fcst_var == strDep1Name & eeStats[[strSeriesVal]] %in% vectValPerms,  ];
+        eeStatsEqualizeUnique = unique(eeStatsEqualize$equalize);
+
+        fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,    eeStatsEqualizeUnique);
+        dfPlot1 = rbind(dfPlot1, fPlot);
+      }
+    }
+
+    if(length(listSeries2Val) > 0){
+      dfPlot2 = data.frame();
+      for( strDep2Name in names(listDep2Plot) ){
+        for(strSeriesVal in names(listSeries2Val)){
+          vectValPerms = c();
+          for(index in 1:length(listSeries2Val[[strSeriesVal]])){
+            vectValPerms= append(vectValPerms, strsplit(listSeries2Val[[strSeriesVal]][index], ",")[[1]]);
+          }
+          fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep2Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
+          eeStatsEqualize = eeStats[eeStats$fcst_var == strDep1Name & eeStats[[strSeriesVal]] %in% vectValPerms,  ];
+          eeStatsEqualizeUnique = unique(eeStatsEqualize$equalize);
+          fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,   eeStatsEqualizeUnique);
+          dfPlot2 = rbind(dfPlot2, fPlot);
+        }
+
+      }
+      dfStatsRec = rbind(dfPlot1, dfPlot2);
+
+    } else{
+      dfStatsRec = dfPlot1;
+    }
   strAfrerEqualizeFile = sub("\\.agg_stat_bootstrap", ".dataAfterEq", strInputDataFile, perl=TRUE);
   write.table(dfStatsRec, file=strAfrerEqualizeFile, quote=FALSE, row.names=FALSE, col.names=TRUE, sep = "\t");
 }
