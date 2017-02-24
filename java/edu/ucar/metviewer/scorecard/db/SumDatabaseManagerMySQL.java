d66ac840e1a91a1070335fd48212e43b54952778
TatianaBurek <tatiana@ucar.edu>
2017-02-24 13:09:00 -0700

    corrected the file name

--- java/edu/ucar/metviewer/scorecard/db/SumDatabaseManagerMySQL.java
+++ java/edu/ucar/metviewer/scorecard/db/SumDatabaseManagerMySQL.java
@@ -1,5 +1,5 @@
 /**
- * CalcDatabaseManagerMySQL.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research (UCAR), National Center for Atmospheric Research
+ * SumDatabaseManagerMySQL.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research (UCAR), National Center for Atmospheric Research
  * (NCAR), Research Applications Laboratory (RAL), P.O. Box 3000, Boulder, Colorado, 80307-3000, USA.Copyright UCAR (c) 2017.
  */
 
@@ -12,11 +12,11 @@ import edu.ucar.metviewer.scorecard.Scorecard;
  * @author : tatiana $
  * @version : 1.0 : 07/02/17 11:38 $
  */
-public class CalcDatabaseManagerMySQL extends DatabaseManagerMySQL {
+public class SumDatabaseManagerMySQL extends DatabaseManagerMySQL {
 
-  public CalcDatabaseManagerMySQL(Scorecard scorecard) {
+  public SumDatabaseManagerMySQL(Scorecard scorecard) {
     super(scorecard);
-    aggStatDataFilePath = scorecard.getWorkingFolders().getDataDir() + scorecard.getDataFile();
+    aggStatDataFilePath = scorecard.getWorkingFolders().getDataDir() + scorecard.getDataFile().replaceAll(".data", ".dataFromDb");
 
   }
 
