e0d514f0e0a13b42d31f0a56cc31f26f27406746
TatianaBurek <tatiana@ucar.edu>
2017-05-18 09:18:16 -0600

    initial version

---
+++ bin/mv_scorecard.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JAVA=/usr/local/jdk/bin/java
+MV_HOME=/d3/projects/METViewer/src/apps/METViewer
+
+# construct the classpath for MVBatch
+CLASSPATH=$MV_HOME/lib/log4j-1.2.15.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/mysql-connector-java-5.1.6.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/xml-apis.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/bonecp-0.7.1.RELEASE.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-api-1.7.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-log4j12-1.7.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/guava-14.0.1.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/j2html-0.7.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-core-2.8.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-databind-2.8.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/commons-lang3-3.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/dist/lib/metviewer.jar
+
+
+$JAVA -classpath $CLASSPATH -Xmx2048M edu.ucar.metviewer.scorecard.Scorecard $@
