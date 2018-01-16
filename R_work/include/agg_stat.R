39d7742e52bc1b725c06449973e4d600c0f8f743
TatianaBurek <tatiana@ucar.edu>
2018-01-16 15:10:45 -0700

    moved all stat calculation into the separate file

--- R_work/include/agg_stat.R
+++ R_work/include/agg_stat.R
@@ -15,6 +15,7 @@ cat("agg_stat.R\ninput file: ", strInputInfoFile, "\n", sep="");
 source(strInputInfoFile);
 setwd(strWorkingDir);
 source("util_plot.R");
+source("statistics.R");
 
 # variables for performance bookkeeping
 stStart			= Sys.time();
@@ -265,411 +266,6 @@ if ( nrow(sampleData) > 0){
   # initialize the histogram array
   listHist = c();
 
-  # SL1L2 stat calculations
-  calcStdDev		= function(sum, sum_sq, n){
-    if ( 1 > n ){
-      return(NA);
-    }
-    v = (sum_sq - sum*sum/n)/(n - 1);
-    if( 0 > v ){
-      return(NA);
-    } else {
-      return( sqrt(v) );
-    }
-  }
-  calcFBAR		= function(d){ return( d$fbar ); }
-  calcOBAR		= function(d){ return( d$obar ); }
-  calcFSTDEV		= function(d){ return( calcStdDev(d$fbar * d$total, d$ffbar * d$total, d$total) ); }
-  calcOSTDEV		= function(d){ return( calcStdDev(d$obar * d$total, d$oobar * d$total, d$total) ); }
-  calcFOBAR		= function(d){ return( d$fobar ); }
-  calcFFBAR		= function(d){ return( d$ffbar ); }
-  calcOOBAR		= function(d){ return( d$oobar ); }
-  calcMAE		= function(d){ return( d$mae ); }
-  calcMBIAS		= function(d){ if( 0 == d$obar ){ return (NA); } else { return( d$fbar / d$obar ); } }
-  calcPR_CORR		= function(d){
-    if( is.na(d$total) || is.na(d$ffbar) || is.na(d$fbar) || is.na(d$oobar) || is.na(d$obar) ){
-      return(NA);
-    } else{
-      v =  (d$total^2 * d$ffbar - d$total^2 * d$fbar^2) * (d$total^2 * d$oobar - d$total^2 * d$obar^2);
-      pr_corr = (d$total^2 * d$fobar - d$total^2 * d$fbar * d$obar) / sqrt(v);
-      if( 0 >= v || 1 < pr_corr ){
-        return(NA);
-      } else {
-        return( pr_corr );
-      }
-    }
-  }
-
-  calcANOM_CORR		= function(d){
-    if( is.na(d$total) || is.na(d$ffbar) || is.na(d$fbar) || is.na(d$oobar) || is.na(d$obar) ){
-      return(NA);
-    } else{
-      v =  (d$total^2 * d$ffbar - d$total^2 * d$fbar^2) * (d$total^2 * d$oobar - d$total^2 * d$obar^2);
-      if( 0 >= v){
-        return(NA);
-      }
-      anom_corr = (d$total^2 * d$fobar - d$total^2 * d$fbar * d$obar) / sqrt(v);
-      if(  1 < anom_corr ){
-        return(NA);
-      } else {
-        return( anom_corr );
-      }
-    }
-  }
-  calcRMSFA		= function(d){
-    if (is.na(d$ffbar) ||  0 > d$ffbar ){
-      return (NA);
-    }
-    return ( sqrt(d$ffbar) );
-  }
-
-  calcRMSOA		= function(d){
-    if (is.na(d$oobar) ||  0 > d$oobar ){
-      return (NA);
-    }
-    return ( sqrt(d$oobar) );
-  }
-  calcME			= function(d){ return( d$fbar - d$obar ); }
-  calcME2			= function(d){
-    me = d$fbar - d$obar;
-    return( me * me );
-  }
-  calcMSE			= function(d){ return( d$ffbar + d$oobar - 2 * d$fobar ); }
-  calcMSESS			= function(d){
-    ostdev=calcOSTDEV( d );
-    mse=calcMSE( d );
-    return( 1.0 - mse/(ostdev*ostdev) );
-  }
-  calcRMSE		= function(d){ return( sqrt(calcMSE(d)) ); }
-  calcESTDEV		= function(d){ return( calcStdDev( calcME(d) * d$total, calcMSE(d) * d$total, d$total) ); }
-  calcBCMSE		= function(d){ return( calcMSE(d) - (d$fbar - d$obar)^2 ); }
-  calcBCRMSE		= function(d){ return( sqrt(calcBCMSE(d)) ); }
-
-  # SSVAR stat calculations
-  calcSSVAR_FBAR		= function(d){ return( calcFBAR(d) ); }
-  calcSSVAR_OBAR		= function(d){ return( calcOBAR(d) ); }
-  calcSSVAR_FSTDEV		= function(d){ return( calcFSTDEV(d) ); }
-  calcSSVAR_OSTDEV		= function(d){ calcOSTDEV(d ); }
-  calcSSVAR_FOBAR		= function(d){ return( calcFOBAR(d) ); }
-  calcSSVAR_FFBAR		= function(d){ return( calcFFBAR(d) ); }
-  calcSSVAR_OOBAR		= function(d){ return( calcOOBAR(d) ); }
-  calcSSVAR_MBIAS		= function(d){ return( calcMBIAS(d) ); }
-  calcSSVAR_PR_CORR		= function(d){ return( calcPR_CORR(d) ); }
-
-  calcSSVAR_ANOM_CORR		= function(d){ return( calcANOM_CORR(d) ); }
-  calcSSVAR_ME			= function(d){ return( calcME(d) ); }
-  calcSSVAR_ME2			= function(d){ return( calcME2(d) ); }
-  calcSSVAR_MSE			= function(d){ return( calcMSE(d) ); }
-  calcSSVAR_MSESS			= function(d){ return( calcMSESS(d) ); }
-  calcSSVAR_RMSE		= function(d){ return( calcRMSE(d) ); }
-  calcSSVAR_ESTDEV		= function(d){ return( calcESTDEV(d) ); }
-  calcSSVAR_BCMSE		= function(d){ return( calcBCMSE(d)); }
-  calcSSVAR_BCRMSE		= function(d){ return( calcBCRMSE(d) ); }
-
-  calcSSVAR_Spread = function(d){
-    if( length( d$varmean ) < 1 ){ return (NA); }
-    return( sqrt(weighted.mean(d$varmean, d$binn)) );
-  }
-
-
-  # CTC stat calculations
-  calcBASER		= function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fn_oy) / d$total ); }             }
-  calcACC			= function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fn_on) / d$total ); }             }
-  calcFBIAS		= function(d){ if( 0 == (d$fy_oy + d$fn_oy) )          { return (NA); } else { return( (d$fy_oy + d$fy_on) / (d$fy_oy + d$fn_oy) ); } }
-  calcFMEAN		= function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fy_on) / d$total ); }             }
-  calcPODY		= function(d){ if( 0 == (d$fy_oy + d$fn_oy) )          { return (NA); } else { return( d$fy_oy / (d$fy_oy + d$fn_oy) ); }             }
-  calcPOFD		= function(d){ if( 0 == (d$fy_on + d$fn_on) )          { return (NA); } else { return( d$fy_on / (d$fy_on + d$fn_on) ); }             }
-  calcPODN		= function(d){ if( 0 == (d$fy_on + d$fn_on) )          { return (NA); } else { return( d$fn_on / (d$fy_on + d$fn_on) ); }             }
-  calcFAR			= function(d){ if( 0 == (d$fy_oy + d$fy_on) )          { return (NA); } else { return( d$fy_on / (d$fy_oy + d$fy_on) ); }             }
-  calcCSI			= function(d){ if( 0 == (d$fy_oy + d$fy_on + d$fn_oy) ){ return (NA); } else { return( d$fy_oy / (d$fy_oy + d$fy_on + d$fn_oy) ); }   }
-  calcGSS = function(d){
-    if( 0 == d$total ){ return (NA); }
-    dblC = ( (d$fy_oy + d$fy_on) / d$total ) * (d$fy_oy + d$fn_oy);
-    gss = ( (d$fy_oy - dblC) / (d$fy_oy + d$fy_on + d$fn_oy - dblC) )
-    return( round(gss, digits=5) );
-  }
-  calcHK = function(d){ if( is.na(calcPODY(d)) || is.na(calcPOFD(d)) ){ return (NA); } else { return( calcPODY(d) - calcPOFD(d) ); } }
-  calcHSS = function(d){
-    if( 0 == d$total ){ return (NA); }
-    #dblC = (
-    #		 as.numeric( (d$fy_oy + d$fy_on)*(d$fy_oy + d$fn_oy) ) +
-    #		 as.numeric( (d$fn_oy + d$fn_on)*(d$fy_on + d$fn_on) )
-    #	   ) / d$total;
-    dblC = ( ((d$fy_oy + d$fy_on) / d$total) *(d$fy_oy + d$fn_oy)  +  ((d$fn_oy + d$fn_on) / d$total) * (d$fy_on + d$fn_on)  ) ;
-    hss = ( (d$fy_oy + d$fn_on - dblC) / (d$total - dblC) );
-    return( round(hss, digits=5) );
-
-  }
-  calcODDS = function(d){
-    if( is.na(calcPODY(d)) || is.na(calcPOFD(d)) ){ return (NA); }
-    dblPOD = calcPODY(d);
-    dblPOFD = calcPOFD(d);
-    return( (dblPOD * (1 - dblPOFD)) / (dblPOFD * (1 - dblPOD)) );
-  }
-  # BAGSS Reference:
-  # Bias Adjusted Precipitation Threat Scores
-  # F. Mesinger, Adv. Geosci., 16, 137-142, 2008
-  calcBAGSS = function(d){
-    if( 0 == d$total ){ return (NA); }
-    dblF  = d$fy_oy + d$fy_on;
-    dblO  = d$fy_oy + d$fn_oy;
-    dblLf = log(dblO / d$fn_oy);
-    dblHa = tryCatch({
-      dblO - (d$fy_on / dblLf) * lambert_W0(dblO / d$fy_on * dblLf);
-    }, warning = function(w) {
-      return (NA)
-    }, error = function(e) {
-      return (NA)
-    });
-    return( (dblHa - (dblO^2 / d$total)) / (2*dblO - dblHa - (dblO^2 / d$total)) );
-  }
-
-calcECLV = function(d){
-    # Build list of X-axis points between 0 and 1
-    CL_PTS = seq(clStep, 1-clStep, clStep)
-    ECLV = value(c(d$fy_oy, d$fy_on, d$fn_oy, d$fn_on), cl = CL_PTS);
-    #find indexses of common steps
-    listInd =ECLV$cl %in% CL_PTS;
-    V = ECLV$V[listInd];
-    return (V);
-  }
-
-
-  # NBR_CNT "calculations"
-  calcNBR_FBS = function(d){ return ( d$fbs ); }
-  calcNBR_FSS = function(d){ return ( d$fss ); }
-  calcNBR_AFSS = function(d){ return ( d$afss ); }
-  calcNBR_UFSS = function(d){ return ( d$ufss ); }
-  calcNBR_F_RATE = function(d){ return ( d$f_rate ); }
-  calcNBR_O_RATE = function(d){ return ( d$o_rate ); }
-
-  #VL1L2 "calculations"
-  calcVL1L2_FBAR = function(d){ return ( sqrt(d$uvffbar) ); }
-  calcVL1L2_OBAR = function(d){ return ( sqrt(d$uvoobar) ); }
-
-  calcVL1L2_BIAS = function(d){
-    bias = sqrt(d$uvffbar) - sqrt(d$uvoobar);
-    return ( round(bias, digits=5)) ;
-  }
-  calcVL1L2_MSE = function(d){
-    mse = d$uvffbar -2 * d$uvfobar + d$uvoobar;
-    if (mse < 0 ){
-      return (NA);
-    }
-    return ( round(mse, digits=5)) ;
-  }
-  calcVL1L2_RMSE = function(d){
-    rmse = sqrt( calcVL1L2_MSE(d ) );
-    return ( round(rmse, digits=5)) ;
-  }
-
-
-  calcVL1L2_FVAR = function(d){
-    fvar = d$uvffbar - d$ufbar^2 - d$vfbar^2 ;
-    return ( round(fvar, digits=5)) ;
-  }
-  calcVL1L2_OVAR = function(d){
-    ovar =  d$uvoobar - d$uobar^2 - d$vobar^2 ;
-    return ( round(ovar, digits=5)) ;
-  }
-  calcVL1L2_FSTDEV = function(d){
-    fstdev = sqrt( calcVL1L2_FVAR (d));
-    return ( round(fstdev, digits=5)) ;
-  }
-  calcVL1L2_OSTDEV = function(d){
-    ostdev = sqrt( calcVL1L2_OVAR (d));
-    return ( round(ostdev, digits=5)) ;
-  }
-  calcVL1L2_FOSTDEV = function(d){
-    ostdev =  sqrt( d$uvffbar - d$ufbar^2 - d$vfbar^2 + d$uvoobar - d$uobar^2 - d$vobar^2 -
-      2*(d$uvfobar - d$ufbar*d$uobar - d$vfbar*d$vobar));
-    return ( round(ostdev, digits=5)) ;
-  }
-  calcVL1L2_COV = function(d){
-    if(  is.na(d$uvfobar) || is.na(d$ufbar) || is.na(d$uobar) || is.na(d$vfbar) || is.na(d$vobar) ){
-     return(NA);
-    } else{
-      cov =    d$uvfobar -d$ufbar*d$uobar - d$vfbar * d$vobar;
-      return ( round(cov, digits=5)) ;
-    }
-  }
-  calcVL1L2_CORR = function(d){
-    return ( calc_wind_corr(d$total, d$ufbar, d$vfbar, d$uobar, d$vobar, d$uvfobar, d$uvffbar, d$uvoobar) );
-  }
-
-  calc_spd = function(u,v){
-    if( is.na(u) || is.na(v) ) {
-      return (NA);
-    }else{
-      return ( sqrt( u^2 + v^2 ) );
-    }
-  }
-
-  calc_dir = function(u,v){
-    tolerance = 1e-5;
-    if( abs(u-0)< tolerance && abs(v-0)< tolerance ){
-      return (NA);
-    } else {
-      return ( atan2d( u, v ) ) ;
-    }
-  }
-
-  calcVL1L2_FSPD = function(d){
-    fspd = calc_spd( d$ufbar, d$vfbar );
-    return ( round(fspd, digits=5)) ;
-  }
-
-  calcVL1L2_OSPD = function(d){
-    ospd = calc_spd( d$uobar, d$vobar );
-    return ( round(ospd, digits=5)) ;
-  }
-
-  calcVL1L2_FDIR = function(d){
-    fdir = calc_dir(-d$ufbar, -d$vfbar);
-
-    if(is.na(fdir)){
-      return (NA);
-    } else {
-      return ( round(fdir, digits=5)) ;
-    }
-  }
-
-  calcVL1L2_ODIR = function(d){
-    odir = calc_dir(-d$uobar, -d$vobar);
-
-    if(is.na(odir)){
-      return (NA);
-    } else {
-      return ( round(odir, digits=5)) ;
-    }
-  }
-
-  calcVL1L2_VDIFF_SPD = function(d){
-    vdiff_spd = calc_spd( d$ufbar-d$uobar, d$vfbar-d$vobar );
-    return ( round(vdiff_spd, digits=5)) ;
-  }
-
-  calcVL1L2_VDIFF_DIR = function(d){
-    vdiff_dir = calc_dir( -(d$ufbar-d$uobar), -(d$vfbar-d$vobar) );
-    return ( round(vdiff_dir, digits=5)) ;
-  }
-
-  calcVL1L2_SPD_ERR = function(d){
-    speed_bias = calcVL1L2_FSPD(d) - calcVL1L2_OSPD(d);
-    return ( round(speed_bias, digits=5)) ;
-  }
-
-  calcVL1L2_SPD_ABSERR = function(d){
-    spd_abserr = abs( calcVL1L2_SPD_ERR(d) );
-    return ( spd_abserr ) ;
-  }
-
-  calcVL1L2_DIR_ABSERR = function(d){
-    ang_btw = abs( calcVL1L2_DIR_ERR(d) );
-    return ( ang_btw ) ;
-  }
-
-  calcVL1L2_DIR_ERR = function(d){
-    f_len = calcVL1L2_FSPD(d);
-    if( !is.na(f_len) && f_len != 0){
-      uf = d$ufbar / f_len;
-      vf = d$vfbar / f_len;
-
-      o_len = calcVL1L2_OSPD(d);
-      uo = d$uobar / o_len;
-      vo = d$vobar / o_len;
-
-      a = vo*uf - uo*vf;
-      b = uo*uf + vo*vf;
-
-      dir_err = calc_dir(a, b);
-    } else{
-      dir_err = NA;
-    }
-
-    if(is.na(dir_err)){
-      return (NA);
-    } else {
-      return ( round(dir_err, digits=5)) ;
-    }
-  }
-
-  #VAL1L2 "calculations"
-  calcVAL1L2_ANOM_CORR = function(d){
-    return ( calc_wind_corr(d$total, d$ufabar, d$vfabar, d$uoabar, d$voabar, d$uvfoabar, d$uvffabar, d$uvooabar) );
-  }
-
-  calc_wind_corr = function( total, uf, vf, uo, vo, uvfo, uvff, uvoo ){
-    if(  is.na(uvfo) || is.na(uf) || is.na(uo) || is.na(vf) || is.na(vo) ){
-         return(NA);
-    } else{
-      corr = ( uvfo - uf * uo - vf * vo ) /
-            ( sqrt( uvff - uf * uf - vf * vf ) * sqrt( uvoo - uo * uo - vo * vo) )
-      return ( round(corr, digits=5)) ;
-    }
-  }
-
-
-  calcPSTD_BRIER = function(d){
-    return ( d$reliability - d$resolution + d$uncertainty );
-  }
-  calcPSTD_BSS_SMPL = function(d){
-    return ( ( d$resolution - d$reliability ) / d$uncertainty );
-  }
-  calcPSTD_BASER = function(d){
-    return ( d$baser );
-  }
-  calcPSTD_RELIABILITY = function(d){
-    return ( d$reliability );
-  }
-  calcPSTD_RESOLUTION = function(d){
-    return ( d$resolution );
-  }
-  calcPSTD_ROC_AUC = function(d){
-    return ( d$roc_auc );
-  }
-
-  calcPSTD_UNCERTAINTY = function(d){
-    return ( d$uncertainty );
-  }
-
-  calcFGBAR = function(d){
-    return  (d$fgbar);
-  }
-
-  calcOGBAR = function(d){
-    return  (d$ogbar);
-  }
-
-  calcMGBAR = function(d){
-    return  (d$mgbar);
-  }
-
-  calcEGBAR = function(d){
-    return  (d$egbar);
-  }
-
-  calcS1 = function(d){
-    if(is.na(d$egbar) || is.na(d$mgbar) || d$mgbar == 0)
-      return ( NA );
-    s1 = 100 * d$egbar / d$mgbar ;
-    return ( round(s1, digits=5) ) ;
-  }
-
-  calcS1_OG = function(d){
-    if(is.na(d$egbar) || is.na(d$ogbar) || d$ogbar == 0)
-      return ( NA );
-    s1_og = 100 * d$egbar / d$ogbar ;
-    return ( round(s1_og, digits=5) ) ;
-  }
-
-  calcFGOG_RATIO = function(d){
-    if(is.na(d$fgbar) || is.na(d$ogbar) || d$ogbar == 0)
-      return ( NA );
-    fgog_ratio = 100 * d$fgbar / d$ogbar ;
-    return ( round(fgog_ratio, digits=5) ) ;
-  }
-
 
 
 
@@ -866,57 +462,67 @@ calcECLV = function(d){
         if( paste(strPerm, "equalize", sep="_") %in% colnames(dfAggPerm) ){
           dfAggPerm = dfAggPerm[ , -which(colnames(dfAggPerm) %in% c(paste(strPerm, "equalize", sep="_")))]
         }
-        oy_i_index = which( colnames(dfAggPerm)== paste(strPerm, "oy_i", sep="_"));
-        on_i_index = which( colnames(dfAggPerm)== paste(strPerm, "on_i", sep="_") );
-        thresh_i_index = oy_i_index - 1;
+        oy_i_index = grep("oy_i", colnames(dfAggPerm), value=FALSE);
+        on_i_index = grep("on_i", colnames(dfAggPerm), value=FALSE);
+        thresh_i_index = grep("thresh_i", colnames(dfAggPerm), value=FALSE);
 
-        for(oy_i in seq( oy_i_index , ncol(dfAggPerm), 3)){
+        for(oy_i in oy_i_index){
           dfAggPerm[1,oy_i] = sum(dfPerm[,oy_i], na.rm = TRUE);
         }
-        for(on_i in seq( on_i_index, ncol(dfAggPerm), 3)){
+        for(on_i in on_i_index){
           dfAggPerm[1,on_i] = sum(dfPerm[,on_i], na.rm = TRUE);
         }
-        dfPctPerm = data.frame(
-          thresh_i	= c( t( dfAggPerm[1,seq( thresh_i_index, ncol(dfAggPerm), 3)] ) ),
-          oy_i		= c( t( dfAggPerm[1,seq( oy_i_index, ncol(dfAggPerm), 3)] ) ),
-          on_i		= c( t( dfAggPerm[1,seq( on_i_index, ncol(dfAggPerm), 3)] ) )
-       );
 
-       # calculate vectors and constants to use below
-       dfPctPerm$n_i = dfPctPerm$oy_i + dfPctPerm$on_i;		# n_j.
-       dfPctPerm = dfPctPerm[0 != dfPctPerm$n_i,];
-       T = sum(dfPctPerm$n_i);									# T
-       oy_total = sum(dfPctPerm$oy_i);							# n_.1
-       o_bar = oy_total / T;									# n_.1 / T
-       dfPctPerm$o_bar_i = dfPctPerm$oy_i / dfPctPerm$n_i;		# o_bar_i
+        if(!is.na(dfAggPerm[1,thresh_i_index][[1]])){
+          dfPctPerm = data.frame(
+            thresh_i	= c( t( dfAggPerm[1,thresh_i_index] ) ),
+            oy_i		= c( t( dfAggPerm[1,oy_i_index] ) ),
+            on_i		= c( t( dfAggPerm[1, on_i_index] ) )
+          );
 
-       # row-based calculations
-       dfPctPerm$oy_tp			= dfPctPerm$oy_i / T;
-       dfPctPerm$on_tp			= dfPctPerm$on_i / T;
-       dfPctPerm$calibration	= dfPctPerm$oy_i / dfPctPerm$n_i;
-       dfPctPerm$refinement	= dfPctPerm$n_i / T;
-       dfPctPerm$likelihood	= dfPctPerm$oy_i / oy_total;
-       dfPctPerm$baserate		= dfPctPerm$o_bar_i;
+           # calculate vectors and constants to use below
+           dfPctPerm$n_i = dfPctPerm$oy_i + dfPctPerm$on_i;		# n_j.
+           dfPctPerm = dfPctPerm[0 != dfPctPerm$n_i,];
+           T = sum(dfPctPerm$n_i);									# T
+           oy_total = sum(dfPctPerm$oy_i);							# n_.1
+           o_bar = oy_total / T;									# n_.1 / T
+           dfPctPerm$o_bar_i = dfPctPerm$oy_i / dfPctPerm$n_i;		# o_bar_i
 
-       # table-based stat calculations
-       dfSeriesSums = list(
-         reliability	= sum( dfPctPerm$n_i * (dfPctPerm$thresh - dfPctPerm$o_bar_i)^2 ) / T,
-         resolution	= sum( dfPctPerm$n_i * (dfPctPerm$o_bar_i - o_bar)^2 ) / T,
-         uncertainty	= o_bar * (1 - o_bar),
-         baser		= o_bar
-       );
-       #dfSeriesSums$b_ci	= calcBrierCI(dfPctPerm, dfSeriesSums$brier, dblAlpha);
+           # row-based calculations
+           dfPctPerm$oy_tp			= dfPctPerm$oy_i / T;
+           dfPctPerm$on_tp			= dfPctPerm$on_i / T;
+           dfPctPerm$calibration	= dfPctPerm$oy_i / dfPctPerm$n_i;
+           dfPctPerm$refinement	= dfPctPerm$n_i / T;
+           dfPctPerm$likelihood	= dfPctPerm$oy_i / oy_total;
+           dfPctPerm$baserate		= dfPctPerm$o_bar_i;
 
-       # build the dataframe for calculating and use the trapezoidal method roc_auc
-       dfROC = calcPctROC(dfPctPerm);
-       dfAUC = rbind(data.frame(thresh=0, n11=0, n10=0, n01=0, n00=0, pody=1, pofd=1), dfROC);
-       dfAUC = rbind(dfAUC, data.frame(thresh=0, n11=0, n10=0, n01=0, n00=0, pody=0, pofd=0));
-       dfSeriesSums$roc_auc = 0;
-       for(r in 2:nrow(dfAUC)){
-         dfSeriesSums$roc_auc = dfSeriesSums$roc_auc + 0.5*(dfAUC[r-1,]$pody + dfAUC[r,]$pody)*(dfAUC[r-1,]$pofd - dfAUC[r,]$pofd);
-       }
+           # table-based stat calculations
+           dfSeriesSums = list(
+             reliability	= sum( dfPctPerm$n_i * (dfPctPerm$thresh - dfPctPerm$o_bar_i)^2 ) / T,
+             resolution	= sum( dfPctPerm$n_i * (dfPctPerm$o_bar_i - o_bar)^2 ) / T,
+             uncertainty	= o_bar * (1 - o_bar),
+             baser		= o_bar
+           );
+           #dfSeriesSums$b_ci	= calcBrierCI(dfPctPerm, dfSeriesSums$brier, dblAlpha);
 
+           # build the dataframe for calculating and use the trapezoidal method roc_auc
+           dfROC = calcPctROC(dfPctPerm);
+           dfAUC = rbind(data.frame(thresh=0, n11=0, n10=0, n01=0, n00=0, pody=1, pofd=1), dfROC);
+           dfAUC = rbind(dfAUC, data.frame(thresh=0, n11=0, n10=0, n01=0, n00=0, pody=0, pofd=0));
+           dfSeriesSums$roc_auc = 0;
+           for(r in 2:nrow(dfAUC)){
+             dfSeriesSums$roc_auc = dfSeriesSums$roc_auc + 0.5*(dfAUC[r-1,]$pody + dfAUC[r,]$pody)*(dfAUC[r-1,]$pofd - dfAUC[r,]$pofd);
+           }
 
+        }else{
+          dfSeriesSums = list(
+            reliability	= NA,
+            resolution	= NA,
+            uncertainty	= NA,
+            baser		= NA,
+            roc_auc=NA
+          );
+        }
 
       }
 
