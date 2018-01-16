6d280307c700bc00f1aa2decb300726a28efec76
TatianaBurek <tatiana@ucar.edu>
2018-01-16 15:11:17 -0700

    new version

---
+++ R_work/include/statistics.R
@@ -0,0 +1,409 @@
+# SL1L2 stat calculations
+  calcStdDev		= function(sum, sum_sq, n){
+    if ( 1 > n ){
+      return(NA);
+    }
+    v = (sum_sq - sum*sum/n)/(n - 1);
+    if( 0 > v ){
+      return(NA);
+    } else {
+      return( sqrt(v) );
+    }
+  }
+calcFBAR    = function(d){ return( d$fbar ); }
+calcOBAR    = function(d){ return( d$obar ); }
+calcFSTDEV    = function(d){ return( calcStdDev(d$fbar * d$total, d$ffbar * d$total, d$total) ); }
+calcOSTDEV    = function(d){ return( calcStdDev(d$obar * d$total, d$oobar * d$total, d$total) ); }
+calcFOBAR    = function(d){ return( d$fobar ); }
+calcFFBAR    = function(d){ return( d$ffbar ); }
+calcOOBAR    = function(d){ return( d$oobar ); }
+calcMAE    = function(d){ return( d$mae ); }
+calcMBIAS    = function(d){ if( 0 == d$obar ){ return (NA); } else { return( d$fbar / d$obar ); } }
+calcPR_CORR    = function(d){
+  if( is.na(d$total) || is.na(d$ffbar) || is.na(d$fbar) || is.na(d$oobar) || is.na(d$obar) ){
+    return(NA);
+  } else{
+    v =  (d$total^2 * d$ffbar - d$total^2 * d$fbar^2) * (d$total^2 * d$oobar - d$total^2 * d$obar^2);
+    pr_corr = (d$total^2 * d$fobar - d$total^2 * d$fbar * d$obar) / sqrt(v);
+    if( 0 >= v || 1 < pr_corr ){
+      return(NA);
+    } else {
+      return( pr_corr );
+    }
+  }
+}
+
+calcANOM_CORR    = function(d){
+  if( is.na(d$total) || is.na(d$ffbar) || is.na(d$fbar) || is.na(d$oobar) || is.na(d$obar) ){
+    return(NA);
+  } else{
+    v =  (d$total^2 * d$ffbar - d$total^2 * d$fbar^2) * (d$total^2 * d$oobar - d$total^2 * d$obar^2);
+    if( 0 >= v){
+      return(NA);
+    }
+    anom_corr = (d$total^2 * d$fobar - d$total^2 * d$fbar * d$obar) / sqrt(v);
+    if(  1 < anom_corr ){
+      return(NA);
+    } else {
+      return( anom_corr );
+    }
+  }
+}
+calcRMSFA    = function(d){
+  if (is.na(d$ffbar) ||  0 > d$ffbar ){
+    return (NA);
+  }
+  return ( sqrt(d$ffbar) );
+}
+
+calcRMSOA    = function(d){
+  if (is.na(d$oobar) ||  0 > d$oobar ){
+    return (NA);
+  }
+  return ( sqrt(d$oobar) );
+}
+calcME      = function(d){ return( d$fbar - d$obar ); }
+calcME2      = function(d){
+  me = d$fbar - d$obar;
+  return( me * me );
+}
+calcMSE      = function(d){ return( d$ffbar + d$oobar - 2 * d$fobar ); }
+calcMSESS      = function(d){
+  ostdev=calcOSTDEV( d );
+  mse=calcMSE( d );
+  return( 1.0 - mse/(ostdev*ostdev) );
+}
+calcRMSE    = function(d){ return( sqrt(calcMSE(d)) ); }
+calcESTDEV    = function(d){ return( calcStdDev( calcME(d) * d$total, calcMSE(d) * d$total, d$total) ); }
+calcBCMSE    = function(d){ return( calcMSE(d) - (d$fbar - d$obar)^2 ); }
+calcBCRMSE    = function(d){ return( sqrt(calcBCMSE(d)) ); }
+
+# SSVAR stat calculations
+calcSSVAR_FBAR    = function(d){ return( calcFBAR(d) ); }
+calcSSVAR_OBAR    = function(d){ return( calcOBAR(d) ); }
+calcSSVAR_FSTDEV    = function(d){ return( calcFSTDEV(d) ); }
+calcSSVAR_OSTDEV    = function(d){ calcOSTDEV(d ); }
+calcSSVAR_FOBAR    = function(d){ return( calcFOBAR(d) ); }
+calcSSVAR_FFBAR    = function(d){ return( calcFFBAR(d) ); }
+calcSSVAR_OOBAR    = function(d){ return( calcOOBAR(d) ); }
+calcSSVAR_MBIAS    = function(d){ return( calcMBIAS(d) ); }
+calcSSVAR_PR_CORR    = function(d){ return( calcPR_CORR(d) ); }
+
+calcSSVAR_ANOM_CORR    = function(d){ return( calcANOM_CORR(d) ); }
+calcSSVAR_ME      = function(d){ return( calcME(d) ); }
+calcSSVAR_ME2      = function(d){ return( calcME2(d) ); }
+calcSSVAR_MSE      = function(d){ return( calcMSE(d) ); }
+calcSSVAR_MSESS      = function(d){ return( calcMSESS(d) ); }
+calcSSVAR_RMSE    = function(d){ return( calcRMSE(d) ); }
+calcSSVAR_ESTDEV    = function(d){ return( calcESTDEV(d) ); }
+calcSSVAR_BCMSE    = function(d){ return( calcBCMSE(d)); }
+calcSSVAR_BCRMSE    = function(d){ return( calcBCRMSE(d) ); }
+
+calcSSVAR_Spread = function(d){
+  if( length( d$varmean ) < 1 ){ return (NA); }
+  return( sqrt(weighted.mean(d$varmean, d$binn)) );
+}
+
+
+# CTC stat calculations
+calcBASER    = function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fn_oy) / d$total ); }             }
+calcACC      = function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fn_on) / d$total ); }             }
+calcFBIAS    = function(d){ if( 0 == (d$fy_oy + d$fn_oy) )          { return (NA); } else { return( (d$fy_oy + d$fy_on) / (d$fy_oy + d$fn_oy) ); } }
+calcFMEAN    = function(d){ if( 0 == d$total )                      { return (NA); } else { return( (d$fy_oy + d$fy_on) / d$total ); }             }
+calcPODY    = function(d){ if( 0 == (d$fy_oy + d$fn_oy) )          { return (NA); } else { return( d$fy_oy / (d$fy_oy + d$fn_oy) ); }             }
+calcPOFD    = function(d){ if( 0 == (d$fy_on + d$fn_on) )          { return (NA); } else { return( d$fy_on / (d$fy_on + d$fn_on) ); }             }
+calcPODN    = function(d){ if( 0 == (d$fy_on + d$fn_on) )          { return (NA); } else { return( d$fn_on / (d$fy_on + d$fn_on) ); }             }
+calcFAR      = function(d){
+  if( 0 == (d$fy_oy + d$fy_on) ){
+    return (NA);
+    } else {
+    return( d$fy_on / (d$fy_oy + d$fy_on) );
+    }             }
+calcCSI      = function(d){ if( 0 == (d$fy_oy + d$fy_on + d$fn_oy) ){ return (NA); } else { return( d$fy_oy / (d$fy_oy + d$fy_on + d$fn_oy) ); }   }
+calcGSS = function(d){
+  if( 0 == d$total ){ return (NA); }
+  dblC = ( (d$fy_oy + d$fy_on) / d$total ) * (d$fy_oy + d$fn_oy);
+  gss = ( (d$fy_oy - dblC) / (d$fy_oy + d$fy_on + d$fn_oy - dblC) )
+  return( round(gss, digits=5) );
+}
+calcHK = function(d){ if( is.na(calcPODY(d)) || is.na(calcPOFD(d)) ){ return (NA); } else { return( calcPODY(d) - calcPOFD(d) ); } }
+calcHSS = function(d){
+  if( 0 == d$total ){ return (NA); }
+  #dblC = (
+  #		 as.numeric( (d$fy_oy + d$fy_on)*(d$fy_oy + d$fn_oy) ) +
+  #		 as.numeric( (d$fn_oy + d$fn_on)*(d$fy_on + d$fn_on) )
+  #	   ) / d$total;
+  dblC = ( ((d$fy_oy + d$fy_on) / d$total) *(d$fy_oy + d$fn_oy)  +  ((d$fn_oy + d$fn_on) / d$total) * (d$fy_on + d$fn_on)  ) ;
+  hss = ( (d$fy_oy + d$fn_on - dblC) / (d$total - dblC) );
+  return( round(hss, digits=5) );
+
+}
+calcODDS = function(d){
+  if( is.na(calcPODY(d)) || is.na(calcPOFD(d)) ){ return (NA); }
+  dblPOD = calcPODY(d);
+  dblPOFD = calcPOFD(d);
+  return( (dblPOD * (1 - dblPOFD)) / (dblPOFD * (1 - dblPOD)) );
+}
+# BAGSS Reference:
+# Bias Adjusted Precipitation Threat Scores
+# F. Mesinger, Adv. Geosci., 16, 137-142, 2008
+calcBAGSS = function(d){
+  if( 0 == d$total || d$fn_oy == 0 || d$fy_on == 0){ return (NA); }
+  dblF  = d$fy_oy + d$fy_on;
+  dblO  = d$fy_oy + d$fn_oy;
+  dblLf = log(dblO / d$fn_oy);
+  dblHa = tryCatch({
+    dblO - (d$fy_on / dblLf) * lambert_W0(dblO / d$fy_on * dblLf);
+  }, warning = function(w) {
+    return (NA)
+  }, error = function(e) {
+    return (NA)
+  });
+  return( (dblHa - (dblO^2 / d$total)) / (2*dblO - dblHa - (dblO^2 / d$total)) );
+}
+
+calcECLV = function(d){
+  # Build list of X-axis points between 0 and 1
+  CL_PTS = seq(clStep, 1-clStep, clStep)
+  ECLV = value(c(d$fy_oy, d$fy_on, d$fn_oy, d$fn_on), cl = CL_PTS);
+  #find indexses of common steps
+  listInd =ECLV$cl %in% CL_PTS;
+  V = ECLV$V[listInd];
+  return (V);
+}
+
+
+# NBR_CNT "calculations"
+calcNBR_FBS = function(d){ return ( d$fbs ); }
+calcNBR_FSS = function(d){ return ( d$fss ); }
+calcNBR_AFSS = function(d){ return ( d$afss ); }
+calcNBR_UFSS = function(d){ return ( d$ufss ); }
+calcNBR_F_RATE = function(d){ return ( d$f_rate ); }
+calcNBR_O_RATE = function(d){ return ( d$o_rate ); }
+
+#VL1L2 "calculations"
+calcVL1L2_FBAR = function(d){ return ( sqrt(d$uvffbar) ); }
+calcVL1L2_OBAR = function(d){ return ( sqrt(d$uvoobar) ); }
+
+calcVL1L2_BIAS = function(d){
+  bias = sqrt(d$uvffbar) - sqrt(d$uvoobar);
+  return ( round(bias, digits=5)) ;
+}
+calcVL1L2_MSE = function(d){
+  mse = d$uvffbar -2 * d$uvfobar + d$uvoobar;
+  if (mse < 0 ){
+    return (NA);
+  }
+  return ( round(mse, digits=5)) ;
+}
+calcVL1L2_RMSE = function(d){
+  rmse = sqrt( calcVL1L2_MSE(d ) );
+  return ( round(rmse, digits=5)) ;
+}
+
+
+calcVL1L2_FVAR = function(d){
+  fvar = d$uvffbar - d$ufbar^2 - d$vfbar^2 ;
+  return ( round(fvar, digits=5)) ;
+}
+calcVL1L2_OVAR = function(d){
+  ovar =  d$uvoobar - d$uobar^2 - d$vobar^2 ;
+  return ( round(ovar, digits=5)) ;
+}
+calcVL1L2_FSTDEV = function(d){
+  fstdev = sqrt( calcVL1L2_FVAR (d));
+  return ( round(fstdev, digits=5)) ;
+}
+calcVL1L2_OSTDEV = function(d){
+  ostdev = sqrt( calcVL1L2_OVAR (d));
+  return ( round(ostdev, digits=5)) ;
+}
+calcVL1L2_FOSTDEV = function(d){
+  ostdev =  sqrt( d$uvffbar - d$ufbar^2 - d$vfbar^2 + d$uvoobar - d$uobar^2 - d$vobar^2 -
+  2*(d$uvfobar - d$ufbar*d$uobar - d$vfbar*d$vobar));
+  return ( round(ostdev, digits=5)) ;
+}
+calcVL1L2_COV = function(d){
+  if(  is.na(d$uvfobar) || is.na(d$ufbar) || is.na(d$uobar) || is.na(d$vfbar) || is.na(d$vobar) ){
+    return(NA);
+  } else{
+    cov =    d$uvfobar -d$ufbar*d$uobar - d$vfbar * d$vobar;
+    return ( round(cov, digits=5)) ;
+  }
+}
+calcVL1L2_CORR = function(d){
+  return ( calc_wind_corr(d$total, d$ufbar, d$vfbar, d$uobar, d$vobar, d$uvfobar, d$uvffbar, d$uvoobar) );
+}
+
+calc_spd = function(u,v){
+  if( is.na(u) || is.na(v) ) {
+    return (NA);
+  }else{
+    return ( sqrt( u^2 + v^2 ) );
+  }
+}
+
+calc_dir = function(u,v){
+  tolerance = 1e-5;
+  if( abs(u-0)< tolerance && abs(v-0)< tolerance ){
+    return (NA);
+  } else {
+    return ( atan2d( u, v ) ) ;
+  }
+}
+
+calcVL1L2_FSPD = function(d){
+  fspd = calc_spd( d$ufbar, d$vfbar );
+  return ( round(fspd, digits=5)) ;
+}
+
+calcVL1L2_OSPD = function(d){
+  ospd = calc_spd( d$uobar, d$vobar );
+  return ( round(ospd, digits=5)) ;
+}
+
+calcVL1L2_FDIR = function(d){
+  fdir = calc_dir(-d$ufbar, -d$vfbar);
+
+  if(is.na(fdir)){
+    return (NA);
+  } else {
+    return ( round(fdir, digits=5)) ;
+  }
+}
+
+calcVL1L2_ODIR = function(d){
+  odir = calc_dir(-d$uobar, -d$vobar);
+
+  if(is.na(odir)){
+    return (NA);
+  } else {
+    return ( round(odir, digits=5)) ;
+  }
+}
+
+calcVL1L2_VDIFF_SPD = function(d){
+  vdiff_spd = calc_spd( d$ufbar-d$uobar, d$vfbar-d$vobar );
+  return ( round(vdiff_spd, digits=5)) ;
+}
+
+calcVL1L2_VDIFF_DIR = function(d){
+  vdiff_dir = calc_dir( -(d$ufbar-d$uobar), -(d$vfbar-d$vobar) );
+  return ( round(vdiff_dir, digits=5)) ;
+}
+
+calcVL1L2_SPD_ERR = function(d){
+  speed_bias = calcVL1L2_FSPD(d) - calcVL1L2_OSPD(d);
+  return ( round(speed_bias, digits=5)) ;
+}
+
+calcVL1L2_SPD_ABSERR = function(d){
+  spd_abserr = abs( calcVL1L2_SPD_ERR(d) );
+  return ( spd_abserr ) ;
+}
+
+calcVL1L2_DIR_ABSERR = function(d){
+  ang_btw = abs( calcVL1L2_DIR_ERR(d) );
+  return ( ang_btw ) ;
+}
+
+calcVL1L2_DIR_ERR = function(d){
+  f_len = calcVL1L2_FSPD(d);
+  if( !is.na(f_len) && f_len != 0){
+    uf = d$ufbar / f_len;
+    vf = d$vfbar / f_len;
+
+    o_len = calcVL1L2_OSPD(d);
+    uo = d$uobar / o_len;
+    vo = d$vobar / o_len;
+
+    a = vo*uf - uo*vf;
+    b = uo*uf + vo*vf;
+
+    dir_err = calc_dir(a, b);
+  } else{
+    dir_err = NA;
+  }
+
+  if(is.na(dir_err)){
+    return (NA);
+  } else {
+    return ( round(dir_err, digits=5)) ;
+  }
+}
+
+#VAL1L2 "calculations"
+calcVAL1L2_ANOM_CORR = function(d){
+  return ( calc_wind_corr(d$total, d$ufabar, d$vfabar, d$uoabar, d$voabar, d$uvfoabar, d$uvffabar, d$uvooabar) );
+}
+
+calc_wind_corr = function( total, uf, vf, uo, vo, uvfo, uvff, uvoo ){
+  if(  is.na(uvfo) || is.na(uf) || is.na(uo) || is.na(vf) || is.na(vo) ){
+    return(NA);
+  } else{
+    corr = ( uvfo - uf * uo - vf * vo ) /
+    ( sqrt( uvff - uf * uf - vf * vf ) * sqrt( uvoo - uo * uo - vo * vo) )
+    return ( round(corr, digits=5)) ;
+  }
+}
+
+
+calcPSTD_BRIER = function(d){
+  return ( d$reliability - d$resolution + d$uncertainty );
+}
+calcPSTD_BSS_SMPL = function(d){
+  return ( ( d$resolution - d$reliability ) / d$uncertainty );
+}
+calcPSTD_BASER = function(d){
+  return ( d$baser );
+}
+calcPSTD_RELIABILITY = function(d){
+  return ( d$reliability );
+}
+calcPSTD_RESOLUTION = function(d){
+  return ( d$resolution );
+}
+calcPSTD_ROC_AUC = function(d){
+  return ( d$roc_auc );
+}
+
+calcPSTD_UNCERTAINTY = function(d){
+  return ( d$uncertainty );
+}
+
+calcFGBAR = function(d){
+  return  (d$fgbar);
+}
+
+calcOGBAR = function(d){
+  return  (d$ogbar);
+}
+
+calcMGBAR = function(d){
+  return  (d$mgbar);
+}
+
+calcEGBAR = function(d){
+  return  (d$egbar);
+}
+
+calcS1 = function(d){
+  if(is.na(d$egbar) || is.na(d$mgbar) || d$mgbar == 0)
+  return ( NA );
+  s1 = 100 * d$egbar / d$mgbar ;
+  return ( round(s1, digits=5) ) ;
+}
+
+calcS1_OG = function(d){
+  if(is.na(d$egbar) || is.na(d$ogbar) || d$ogbar == 0)
+  return ( NA );
+  s1_og = 100 * d$egbar / d$ogbar ;
+  return ( round(s1_og, digits=5) ) ;
+}
+
+calcFGOG_RATIO = function(d){
+  if(is.na(d$fgbar) || is.na(d$ogbar) || d$ogbar == 0)
+  return ( NA );
+  fgog_ratio = 100 * d$fgbar / d$ogbar ;
+  return ( round(fgog_ratio, digits=5) ) ;
+}
---
+++ R_work/include/sum_stat.R
@@ -0,0 +1,205 @@
+library(parallel)
+library(data.table)
+
+
+strInputInfoFile = "~/plot_00124_20130923_082001.agg_stat.info";
+
+listArgs = commandArgs(TRUE)
+if( 0 <  length(listArgs) ) {
+	strInputInfoFile = listArgs[1];
+}
+cat("sum_stat.R\ninput file: ", strInputInfoFile, "\n", sep="");
+
+source(strInputInfoFile);
+setwd(strWorkingDir);
+source("util_plot.R");
+source("statistics.R");
+
+
+prepareCalc = function(d){
+  row = setNames(as.list(d), names(dfStatsRec));
+  dblStat = do.call( paste("calc", row$stat_name, sep=""), list(d=lapply(row[statFields], as.numeric)) );
+  if(!is.na(dblStat)){
+    row$stat_value = round(dblStat, 6);
+    for(field in  statFields){
+      row[[field]] = NULL;
+    }
+    if( !is.null(row$equalize) ){
+      row$equalize = NULL;
+    }
+    fwrite(row, file=strOutputFile,append=TRUE,quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE, na = "NA");
+  }
+}
+
+# variables for performance bookkeeping
+stStart      = Sys.time();
+
+if ( boolSumCtc    ){
+  statFields = c("total", "fy_oy", "fy_on", "fn_oy", "fn_on");
+} else if( boolSumSl1l2  ){
+  statFields = c("total", "fbar", "obar", "fobar", "ffbar", "oobar", "mae");
+} else if( boolSumGrad  ){
+  statFields = c("total", "fgbar", "ogbar", "mgbar", "egbar");
+} else if( boolSumSal1l2  ){
+  statFields = c("total", "fabar", "oabar", "foabar", "ffabar", "ooabar", "mae");
+} else if( boolSumVl1l2  ){
+  statFields = c("total", "ufbar", "vfbar", "uobar", "vobar", "uvfobar", "uvffbar","uvoobar");
+}
+
+# read the input data file into a data frame
+#if fcst_var is a special char - read it as-is and do not convert
+sampleData = read.delim(strInputDataFile,nrows=5);
+ind = names(sampleData) %in% statFields;
+
+newColumns = t(names(sampleData)[!ind]);
+write.table(newColumns, file=strOutputFile, row.names=FALSE, quote=FALSE, sep="\t", append=FALSE, col.names=FALSE);
+
+if ( nrow(sampleData) > 0){
+  classes <- sapply(sampleData, class);
+  numeric_columns <- c('stat_value', 'stat_bcl', 'stat_bcu', 'stat_ncu', 'stat_ncl','fbs', 'fss', 'fbar',  'obar',  'fobar',  'ffbar',  'oobar',  'var_mean');
+  integer_columns <- c('fcst_lead','nstats');
+  character_columns <- c('fcst_var', 'model', 'stat_name')
+  for(col in 1:ncol(sampleData)){
+    if ( is.element( names(classes)[col] , numeric_columns) ){
+      classes[col]='numeric';
+    } else if ( is.element( names(classes)[col] , integer_columns) ){
+      classes[col]='integer';
+    } else if ( is.element( names(classes)[col] , character_columns) ){
+      classes[col]='character';
+    }
+  }
+
+  dfStatsRec = fread(strInputDataFile,colClasses = classes,data.table=FALSE);
+
+  intYMax = 1;
+  if( 0 < length(listSeries2Val) ){ intYMax = 2; }
+
+  # sort the dataset by init time, lead time and independent variable
+  listFields = names(dfStatsRec);
+
+  listGroupToValue=c();
+  if(length(names(listSeries1Val)) > 1){
+    for (index  in 1: length(names(listSeries1Val)) ){
+      strSeriesVar=names(listSeries1Val)[[index]];
+      valSeries = listSeries1Val[[strSeriesVar]];
+      for(strVar in valSeries){
+        if( grepl(',', strVar)) {
+          newName = paste('Group_y1_',index,sep = "");
+          listGroupToValue[[newName]]= strVar;
+        }
+      }
+    }
+  }
+
+  if(length(names(listSeries2Val)) > 1){
+    for (index  in 1: length(names(listSeries2Val)) ){
+      strSeriesVar=names(listSeries2Val)[[index]];
+      valSeries = listSeries2Val[[strSeriesVar]];
+      for(strVar in valSeries){
+        if( grepl(',', strVar)) {
+          newName = paste('Group_y2_',index,sep = "");
+          listGroupToValue[[newName]]= strVar;
+        }
+      }
+    }
+  }
+
+
+  dfStatsRecAxis1= data.frame();
+  dfStatsRecAxis2= data.frame();
+  # build a list for output permutations
+
+
+  # run event equalizer, if requested
+  if( boolEventEqual  ){
+    boolMulti=FALSE;
+    #run event equalizer on Y1
+    dfPlot1 = data.frame();
+    #list all fixed variables
+    listFixVars=c();
+    listFixVarVals = c();
+    if(length(listFixedValEx) > 0){
+      for(index in 1:length(listFixedValEx)){
+        if(is.null(listFixVars)){
+          listFixVars = names(listFixedValEx[index]);
+          listFixVarVals = permute(listFixedValEx[index]);
+        }else{
+          listFixVars =  append(listFixVars, names(listFixedValEx[index]));
+          listFixVarVals = append(listFixVarVals, permute(listFixedValEx[index]));
+        }
+      }
+    }
+    for( strDep1Name in names(listDep1Plot) ){
+      for( strDep1Stat in listDep1Plot[[strDep1Name]] ){
+        fPlot = dfStatsRec;
+        for(strSeriesVal in names(listSeries1Val)){
+          vectValPerms = c();
+          for(index in 1:length(listSeries1Val[[strSeriesVal]])){
+            vectValPerms= append(vectValPerms, strsplit(listSeries1Val[[strSeriesVal]][index], ",")[[1]]);
+          }
+          fPlot = fPlot[fPlot$fcst_var == strDep1Name & fPlot[[strSeriesVal]] %in% vectValPerms & fPlot$stat_name %in% strDep1Stat,  ];
+        }
+
+        fPlot = eventEqualize(fPlot, strIndyVar, listIndyVal, listSeries1Val, listFixVars,listFixVarVals, boolEqualizeByIndep, boolMulti);
+        dfPlot1 = rbind(dfPlot1, fPlot);
+      }
+    }
+
+
+    #if the second Y axis is present - run event equalizer on Y1
+    # and then run event equalizer on Y1 and Y2 equalized data
+    if(length(listSeries2Val) > 0){
+      dfPlot2 = data.frame();
+      for( strDep2Name in names(listDep2Plot) ){
+        for( strDep2Stat in listDep2Plot[[strDep2Name]] ){
+          fPlot = dfStatsRec;
+          for(strSeriesVal in names(listSeries2Val)){
+            vectValPerms = c();
+            for(index in 1:length(listSeries2Val[[strSeriesVal]])){
+              vectValPerms= append(vectValPerms, strsplit(listSeries2Val[[strSeriesVal]][index], ",")[[1]]);
+            }
+            fPlot = fPlot[fPlot$fcst_var == strDep1Name & fPlot[[strSeriesVal]] %in% vectValPerms & fPlot$stat_name %in% strDep2Stat,  ];
+          }
+
+          fPlot = eventEqualize(fPlot, strIndyVar, listIndyVal, listSeries2Val, listFixVars,listFixVarVals, boolEqualizeByIndep, boolMulti);
+          dfPlot2 = rbind(dfPlot2, fPlot);
+        }
+      }
+
+      dfStatsRec = rbind(dfPlot1, dfPlot2);
+      listSeriesVal=list();
+      listSeriesVal$model = append(listSeries1Val$model,listSeries2Val$model)
+      for( seriesVal in names(listSeries1Val) ){
+        listSeriesVal[[seriesVal]] = unique(append(listSeries1Val[[seriesVal]], listSeries2Val[[seriesVal]]));
+      }
+      dfStatsRec = eventEqualize(dfStatsRec, strIndyVar, listIndyVal, listSeriesVal, listFixVars,listFixVarVals,boolEqualizeByIndep, TRUE);
+    }else{
+      dfStatsRec = dfPlot1;
+    }
+    strAfrerEqualizeFile = sub("\\.agg_stat", ".dataAfterEq", strInputDataFile, perl=TRUE);
+    fwrite(dfStatsRec, file=strAfrerEqualizeFile, quote=FALSE, row.names=FALSE, col.names=TRUE,
+    sep = "\t", fwrite);
+  }
+
+
+  if( 1 > nrow(dfStatsRec) ){
+    if( boolEventEqual  ){
+      stop("ERROR: eventEqualize() removed all data");
+    }else{
+      stop("ERROR: Database query returned empy set");
+    }
+  }
+
+  dfStatsRecTranspose=data.frame(t(dfStatsRec),stringsAsFactors = FALSE);
+  a=mclapply( dfStatsRecTranspose ,prepareCalc, mc.cores=6, mc.set.seed=1);
+  #a=lapply( dfStatsRecTranspose,prepareCalc);
+
+
+
+}
+cat(
+  "   total time: ", formatTimeSpan(as.numeric(Sys.time() - stStart, units="secs")), "\n",
+  sep="");
+
+# clean up
+cat("sum_stat.R done\n");
