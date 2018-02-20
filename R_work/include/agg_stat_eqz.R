7fbf478c44b8419c3d7b8bb69f47414f943ce2be
TatianaBurek <tatiana@ucar.edu>
2018-02-20 14:30:30 -0700

    preread data to get correct data types

--- R_work/include/agg_stat_eqz.R
+++ R_work/include/agg_stat_eqz.R
@@ -13,54 +13,74 @@ source(strInputInfoFile);
 setwd(strWorkingDir);
 source("util_plot.R");
 
-# read the plot data
-dfStatsRec = read.delim(strInputDataFile);
 
-if(boolEventEqual){
-  eeStats = read.delim(strInputEeDataFile);
-  dfPlot1 = data.frame();
-
-  for( strDep1Name in names(listDep1Plot) ){
-    for(strSeriesVal in names(listSeries1Val)){
-      vectValPerms = c();
-      for(index in 1:length(listSeries1Val[[strSeriesVal]])){
-        vectValPerms= append(vectValPerms, strsplit(listSeries1Val[[strSeriesVal]][index], ",")[[1]]);
-      }
-      fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep1Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
-      eeStatsEqualize = eeStats[eeStats$fcst_var == strDep1Name & eeStats[[strSeriesVal]] %in% vectValPerms,  ];
-      eeStatsEqualizeUnique = unique(eeStatsEqualize$equalize);
-
-      fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,    eeStatsEqualizeUnique);
-      dfPlot1 = rbind(dfPlot1, fPlot);
+#read the input data file into a data frame
+#if fcst_var is a special char - read it as-is and do not convert
+sampleData = read.delim(strInputDataFile,nrows=5);
+if ( nrow(sampleData) > 0){
+  classes <- sapply(sampleData, class);
+  numeric_columns <- c('stat_value');
+  integer_columns <- c('fcst_lead');
+  character_columns <- c('fcst_var', 'model', 'stat_name', 'object_id', 'cluster_id');
+  for(col in 1:ncol(sampleData)){
+    if ( is.element( names(classes)[col] , numeric_columns) ){
+      classes[col]='numeric';
+    } else if ( is.element( names(classes)[col] , integer_columns) ){
+      classes[col]='integer';
+    } else if ( is.element( names(classes)[col] , character_columns) ){
+      classes[col]='character';
     }
   }
 
-  if(length(listSeries2Val) > 0){
-    dfPlot2 = data.frame();
-    for( strDep2Name in names(listDep2Plot) ){
-      for(strSeriesVal in names(listSeries2Val)){
+  # read the plot data
+  dfStatsRec = read.delim(strInputDataFile,colClasses = classes);
+
+  if(boolEventEqual){
+    eeStats = read.delim(strInputEeDataFile);
+    dfPlot1 = data.frame();
+
+    for( strDep1Name in names(listDep1Plot) ){
+      for(strSeriesVal in names(listSeries1Val)){
         vectValPerms = c();
-        for(index in 1:length(listSeries2Val[[strSeriesVal]])){
-          vectValPerms= append(vectValPerms, strsplit(listSeries2Val[[strSeriesVal]][index], ",")[[1]]);
+        for(index in 1:length(listSeries1Val[[strSeriesVal]])){
+          vectValPerms= append(vectValPerms, strsplit(listSeries1Val[[strSeriesVal]][index], ",")[[1]]);
         }
-        fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep2Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
+        fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep1Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
         eeStatsEqualize = eeStats[eeStats$fcst_var == strDep1Name & eeStats[[strSeriesVal]] %in% vectValPerms,  ];
         eeStatsEqualizeUnique = unique(eeStatsEqualize$equalize);
-        fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,   eeStatsEqualizeUnique);
-        dfPlot2 = rbind(dfPlot2, fPlot);
+
+        fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,    eeStatsEqualizeUnique);
+        dfPlot1 = rbind(dfPlot1, fPlot);
       }
-
     }
-    dfStatsRec = rbind(dfPlot1, dfPlot2);
 
-  } else{
-    dfStatsRec = dfPlot1;
+    if(length(listSeries2Val) > 0){
+      dfPlot2 = data.frame();
+      for( strDep2Name in names(listDep2Plot) ){
+        for(strSeriesVal in names(listSeries2Val)){
+          vectValPerms = c();
+          for(index in 1:length(listSeries2Val[[strSeriesVal]])){
+            vectValPerms= append(vectValPerms, strsplit(listSeries2Val[[strSeriesVal]][index], ",")[[1]]);
+          }
+          fPlot = dfStatsRec[dfStatsRec$fcst_var == strDep2Name & dfStatsRec[[strSeriesVal]] %in% vectValPerms,  ];
+          eeStatsEqualize = eeStats[eeStats$fcst_var == strDep1Name & eeStats[[strSeriesVal]] %in% vectValPerms,  ];
+          eeStatsEqualizeUnique = unique(eeStatsEqualize$equalize);
+          fPlot = eventEqualizeAgainstValues(fPlot, strIndyVar,   eeStatsEqualizeUnique);
+          dfPlot2 = rbind(dfPlot2, fPlot);
+        }
+
+      }
+      dfStatsRec = rbind(dfPlot1, dfPlot2);
+
+    } else{
+      dfStatsRec = dfPlot1;
+    }
+
   }
 
+
+
+  write.table( dfStatsRec, file=strOutputFile, row.names=FALSE, quote=FALSE, sep="\t" );
 }
-
-
-
-write.table( dfStatsRec, file=strOutputFile, row.names=FALSE, quote=FALSE, sep="\t" );
 
 
