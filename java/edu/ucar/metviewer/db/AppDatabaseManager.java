3904d9a4972d83a844dead1d4358a44372e579f7
TatianaBurek <tatiana@ucar.edu>
2018-05-21 09:12:52 -0600

    cleanup

--- java/edu/ucar/metviewer/db/AppDatabaseManager.java
+++ java/edu/ucar/metviewer/db/AppDatabaseManager.java
@@ -1,6 +1,7 @@
 /**
- * AppDatabaseManager.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research (UCAR), National Center for Atmospheric Research (NCAR),
- * Research Applications Laboratory (RAL), P.O. Box 3000, Boulder, Colorado, 80307-3000, USA.Copyright UCAR (c) 2017.
+ * AppDatabaseManager.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research
+ * (UCAR), National Center for Atmospheric Research (NCAR), Research Applications Laboratory (RAL),
+ * P.O. Box 3000, Boulder, Colorado, 80307-3000, USA.Copyright UCAR (c) 2017.
  */
 
 package edu.ucar.metviewer.db;
@@ -21,16 +22,32 @@ import edu.ucar.metviewer.MVPlotJob;
  */
 public interface AppDatabaseManager {
 
-  List<String> buildPlotSql(MVPlotJob job, MVOrderedMap mapPlotFixPerm,PrintWriter printStreamSql) throws Exception;
+  List<String> buildPlotSql(
+                               MVPlotJob job, MVOrderedMap mapPlotFixPerm,
+                               PrintWriter printStreamSql) throws Exception;
 
-  List<String> buildPlotModeEventEqualizeSql(MVPlotJob job, MVOrderedMap mapPlotFixPerm, MVOrderedMap mapPlotFixVal) throws Exception;
+  List<String> buildPlotModeEventEqualizeSql(
+                                                MVPlotJob job, MVOrderedMap mapPlotFixPerm,
+                                                MVOrderedMap mapPlotFixVal) throws Exception;
 
-  boolean executeQueriesAndSaveToFile(List<String> queries, String fileName, boolean isCalc, String currentDBName, boolean isNewFile) throws Exception;
+  boolean executeQueriesAndSaveToFile(
+                                         List<String> queries, String fileName, boolean isCalc,
+                                         String currentDBName, boolean isNewFile) throws Exception;
 
-  String buildAndExecuteQueriesForHistJob(MVPlotJob job, String strDataFile, MVOrderedMap listPlotFixPerm,  PrintStream printStream, PrintWriter printStreamSql) throws Exception;
+  String buildAndExecuteQueriesForHistJob(
+                                             MVPlotJob job, String strDataFile,
+                                             MVOrderedMap listPlotFixPerm, PrintStream printStream,
+                                             PrintWriter printStreamSql) throws Exception;
 
-  int buildAndExecuteQueriesForRocRelyJob(MVPlotJob job, String strDataFile, MVOrderedMap listPlotFixPerm,  PrintStream printStream, PrintWriter printStreamSql) throws Exception;
-  int buildAndExecuteQueriesForEclvJob(MVPlotJob job, String strDataFile, MVOrderedMap listPlotFixPerm,  PrintStream printStream, PrintWriter printStreamSql) throws Exception;
+  int buildAndExecuteQueriesForRocRelyJob(
+                                             MVPlotJob job, String strDataFile,
+                                             MVOrderedMap listPlotFixPerm, PrintStream printStream,
+                                             PrintWriter printStreamSql) throws Exception;
+
+  int buildAndExecuteQueriesForEclvJob(
+                                          MVPlotJob job, String strDataFile,
+                                          MVOrderedMap listPlotFixPerm, PrintStream printStream,
+                                          PrintWriter printStreamSql) throws Exception;
 
   DatabaseInfo getDatabaseInfo();
 
@@ -43,9 +60,9 @@ public interface AppDatabaseManager {
   List<String> getAllDatabases();
 
   void initDBList();
+
   public SimpleDateFormat getDateFormat();
 
   public abstract Pattern getDateRangePattern();
-
 
 }
