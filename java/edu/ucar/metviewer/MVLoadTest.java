4072f58c73a68c817bce5cbb2c09175053e107ba
Venita Hagerty <venita.hagerty@noaa.gov>
2018-05-23 12:13:13 -0600

    Continued test debugging for MVBatch

--- java/edu/ucar/metviewer/MVLoadTest.java
+++ java/edu/ucar/metviewer/MVLoadTest.java
@@ -1,12 +1,32 @@
 package edu.ucar.metviewer;
 
+import edu.ucar.metviewer.db.DatabaseInfo;
+import edu.ucar.metviewer.db.LoadDatabaseManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.ucar.metviewer.test.util.TestUtil.FILE_SEPARATOR;
+import static edu.ucar.metviewer.test.util.TestUtil.ROOT_DIR;
 import static org.junit.Assert.*;
 
 public class MVLoadTest {
+    static MVLoad mvLoad;
+    static String testDataDir = ROOT_DIR + FILE_SEPARATOR + "met_data";
+    static ByteArrayOutputStream log;
+    static PrintStream printStream;
+    static ByteArrayOutputStream logSql;
+    static ByteArrayOutputStream logError;
+    static PrintWriter printStreamSql;
+    static PrintStream printStreamError;
+    static DatabaseInfo databaseInfo;
+    static LoadDatabaseManager databaseManager;
 
     @Before
     public void setUp() throws Exception {
@@ -22,6 +42,9 @@ public class MVLoadTest {
 
     @Test
     public void getUsage() {
+        List<String> argsList = new ArrayList<>();
+        argsList.add("-help");  //invalid option
+        MVLoad.main(argsList.toArray(new String[argsList.size()]));
     }
 
     @Test
