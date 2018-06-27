88204db6cd4c59180ef9dee045b7ad770adc526a
ambverif <ambverif.gsd@noaa.gov>
2018-06-27 19:23:17 +0000

    mvtest

--- bin/mv_test.sh
+++ bin/mv_test.sh
@@ -1,11 +1,78 @@
 #!/bin/bash
 
-JAVA=/usr/local/jdk/bin/java
-JAVA=$(which java)
+usage() { echo "Usage: $0 [-m <path to METViewer home>] [-t <path to METViewer test directory>] [-d <mv_database>] [-u <mv_user>] [-p mv_passwd] [-h <mv_host>] [-P <mv_port>] [-j <path to java executible>] [-c(capture created images)] [-n(no clean)] [test_directory [all|plots]]" 1>&2; exit 1; }
+export mv_database="mv_test"
+export mv_user="mvuser"
+export mv_pwd="mvuser"
+export mv_host="dakota.rap.ucar.edu"
+#export mv_host="model-vxtest.gsd.esrl.noaa.gov"
+export mv_port=3306
+export NOCLEAN=""
+export CAPTURE_CREATED_IMAGES=""
+while getopts "m:t:d:u:p:P:h:j:cn?" o; do
+    case "${o}" in
+        m)
+			if [ ! -d "${OPTARG}" ]; then
+				echo "directory ${OPTARG} does not exist"
+				usage
+			fi
+            export MV_HOME=${OPTARG}
+            ;;
+        t)
+			if [ ! -d "${OPTARG}" ]; then
+				echo "directory ${OPTARG} does not exist"
+				usage
+			fi
+            export MV_TEST_HOME=${OPTARG}
+            ;;
+        d)
+            MV_DATABASE=${OPTARG}
+            ;;
+        u)
+            MV_USER=${OPTARG}
+            ;;
+        p)
+            MV_PASSWD=${OPTARG}
+            ;;
+        P)
+            MV_PORT=${OPTARG}
+            ;;
+        h)
+            MV_HOST=${OPTARG}
+            ;;
+        j)
+			if [ ! -x "${OPTARG}" ]; then
+				echo "file ${OPTARG} does not exist or is not executible"
+				usage
+			fi
+            export JAVA=${OPTARG}
+			$JAVA -version
+			if [ "$?" -ne "0" ]; then
+			   echo "You provided a bad java executible";
+			   exit 1;
+			fi
+            ;;
+        c)
+            CAPTURE_CREATED_IMAGES="yes"
+            ;;
+        n)
+            NOCLEAN="yes"
+            ;;
+		?) 
+		usage
+		;;
+    esac
+done
+shift $((OPTIND-1))
+echo "remaining params $*"
+
+if [ -z ${JAVA+x} ]; then 
+	JAVA=$(which java)
+fi
 $JAVA -version
 if [ "$?" -ne "0" ]; then
    echo "You do not have a java executible in your path";
-   exit;
+   exit 1;
 fi
 
 if [ -z ${MV_HOME+x} ]; then 
@@ -13,7 +80,16 @@ if [ -z ${MV_HOME+x} ]; then
 	echo "setting it to /d3/projects/METViewer/src_dev/apps/METViewer"
 	MV_HOME=/d3/projects/METViewer/src_dev/apps/METViewer
 else
-	echo "MV_HOME is set to '$MV_HOME'"
+	echo "MV_HOME is set to ${MV_HOME}"
+fi
+
+
+if [ -z ${MV_TEST_HOME+x} ]; then 
+	echo "MV_HOME is unset"
+	echo "setting it to /d3/projects/METViewer/src_dev/apps/METViewer"
+	MV_HOME=/d3/projects/METViewer/src_dev/apps/METViewer
+else
+	echo "MV_HOME is set to ${MV_HOME}"
 fi
 
 # construct the classpath for MVLoad
@@ -37,10 +113,13 @@ CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-core-2.10.0.jar
 CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-iostreams-2.10.0.jar
 CLASSPATH=$CLASSPATH:$MV_HOME/dist/lib/metviewer_all.jar
 
-JAVA_OPTS="-Xmx2048M -ea -Dmv_root_dir=/Users/pierce/test_data -Dmv_database=mv_test -Dmv_user=mvuser -Dmv_pwd=mvuser-P@\$\$2018 -Dmv_host=model-vxtest.gsd.esrl.noaa.gov -Dmv_port=3306"
+#-Dmv_root_dir=${HOME}/test_data -Dmv_database=mv_test_rp -Dmv_user=met_admin -Dmv_pwd=MaPass4mvmay2018## -Dmv_host=model-vxtest.gsd.esrl.noaa.gov -Dlog4j.configurationFile=file:${HOME}/IdeaProjects/METViewer/java/edu/ucar/metviewer/resources/log4j2.xml -Dmv_port=3306 -DnoClean -DcaptureCreatedImages=yes
+#/Users/pierce/test_data all
+JAVA_OPTS="-Xmx2048M -ea -Dmv_root_dir=$MV_TEST_HOME -Dmv_database=$MV_DATABASE -Dmv_user=$MV_USER -Dmv_pwd=$MV_PASSWD -Dmv_host=$MV_HOST -Dmv_port=$MV_PORT"
 echo "JAVA_OPTS ARE ... $JAVA_OPTS"
 echo "---------"
 
+cd $MV_HOME
 if [ $# -eq 0 ]
 then
   echo "Running plots scripts - no params - $*"
@@ -56,15 +135,13 @@ else
     $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner "$dir"
 
   else
-    if [[ (2 -eq $#) &&  ($2 == "all") ]];
-    then
-      echo "Running all scripts - 2 params $dir (all) - $*"
+    if [[ (2 -eq $#) &&  ($2 == "all") ]]; then
+      echo "Running all scripts - 2 params $dir \(all\) - $*"
       echo $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner "$dir" all
       $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner "$dir" all
     else
-      if [[ (2 -eq $#) && ($2 == "plots") ]];
-      then
-        echo "Running plots scripts - 2 params (plots) - $*"
+      if [[ (2 -eq $#) && ($2 == "plots") ]]; then
+        echo "Running plots scripts - 2 params \(plots\) - $*"
         echo $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner
         $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner
       fi
