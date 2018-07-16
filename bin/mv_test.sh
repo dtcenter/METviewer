ed61be65a9f962db421e1b5cfa29a11384154727
pierce <randy.pierce@noaa.gov>
2018-07-16 16:37:27 -0600

    bad link for met_data in autotest

--- bin/mv_test.sh
+++ bin/mv_test.sh
@@ -96,31 +96,35 @@ else
 fi
 
 # construct the classpath for MVLoad
-CLASSPATH=$MV_HOME/lib/log4j-1.2.15.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/mysql-connector-java-5.1.6.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/xercesImpl.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/xml-apis.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/tomcat-jdbc-8.5.2.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/juli-6.0.53.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-api-1.7.5.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/slf4j-log4j12-1.7.5.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/guava-14.0.1.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/junit-4.11.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/hamcrest-core-1.3.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/commons-io-2.4.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/mockito-all-1.9.5.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/servlet-api.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/commons-fileupload-1.3.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-api-2.10.0.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-core-2.10.0.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-iostreams-2.10.0.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/dist/lib/metviewer_all.jar
+CLASSPATH=${MV_HOME}/lib/log4j-1.2.15.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/mysql-connector-java-5.1.6.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/xml-apis.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/tomcat-jdbc-8.5.2.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/juli-6.0.53.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-api-1.7.5.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-log4j12-1.7.5.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/guava-14.0.1.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/junit-4.11.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/hamcrest-core-1.3.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-io-2.4.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/mockito-all-1.9.5.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/servlet-api.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-fileupload-1.3.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-api-2.10.0.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-core-2.10.0.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-iostreams-2.10.0.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/dist/lib/metviewer_all.jar
 
 echo "Running allRestRunner"
 #/Users/pierce/test_data all
 JAVA_OPTS="-Xmx2048M -ea -Dmv_root_dir=$MV_TEST_HOME -Dmv_database=$MV_DATABASE -Dmv_user=$MV_USER -Dmv_pwd=$MV_PASSWD -Dmv_host=$MV_HOST -Dmv_port=$MV_PORT -Dlog4j.configurationFile=file:${MV_HOME}/java/edu/ucar/metviewer/resources/log4j2.xml $CAPTURE_CREATED_IMAGES $NOCLEAN $LOADDATA"
 echo "---------"
-cd $MV_HOME
+cd ${MV_HOME}
+#echo "*******"
+#echo $JAVA_OPTS
+#echo "*******"
+
 echo $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner
 $JAVA -classpath $CLASSPATH $JAVA_OPTS edu.ucar.metviewer.test.AllTestRunner
 echo "---------"
