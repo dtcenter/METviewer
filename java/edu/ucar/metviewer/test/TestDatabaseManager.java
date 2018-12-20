f033a187b39220fb9e964370d1943556ec8f51f2
TatianaBurek <tatiana@ucar.edu>
2018-12-20 13:33:29 -0700

    add MariaDB and Aurora databases

--- java/edu/ucar/metviewer/test/TestDatabaseManager.java
+++ java/edu/ucar/metviewer/test/TestDatabaseManager.java
@@ -1,33 +1,52 @@
 /**
- * DatabaseManager.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research (UCAR), National Center for Atmospheric Research (NCAR),
- * Research Applications Laboratory (RAL), P.O. Box 3000, Boulder, Colorado, 80307-3000, USA.Copyright UCAR (c) 2017.
+ * DatabaseManager.java Copyright UCAR (c) 2017. University Corporation for Atmospheric Research
+ * (UCAR), National Center for Atmospheric Research (NCAR), Research Applications Laboratory (RAL),
+ * P.O. Box 3000, Boulder, Colorado, 80307-3000, USA.Copyright UCAR (c) 2017.
  */
 
 package edu.ucar.metviewer.test;
 
+import edu.ucar.metviewer.db.AuroraAppDatabaseManager;
 import edu.ucar.metviewer.db.DatabaseInfo;
+import edu.ucar.metviewer.db.MariaDbAppDatabaseManager;
+import edu.ucar.metviewer.db.MysqlDatabaseManager;
 
 /**
  * @author : tatiana $
  * @version : 1.0 : 19/05/17 12:42 $
  */
 public class TestDatabaseManager {
-    public static TestDBManager getManager(String management_system, String host, String user, String password, String dbName) throws Exception {
-        String ms = management_system.toLowerCase();
-        String dbType = (ms == null || ms == "") ? "mysql" : ms; // default dbType to mysql if management_system is missing
-        DatabaseInfo databaseInfo = new DatabaseInfo(host, user, password);
-        databaseInfo.setDbName(dbName);
-        TestDBManager databaseManager = null;
-        switch (dbType) {
-            case "mysql":
-                databaseManager = new TestMysqlDatabaseManager(databaseInfo);
-                break;
-            case "cb":
-                databaseManager = new TestCBDatabaseManager(databaseInfo);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid database type: " + dbType);
-        }
-        return databaseManager;
+
+  public static TestDBManager getManager(
+      String management_system, String host, String user, String password,
+      String dbName) throws Exception {
+    String ms = management_system.toLowerCase();
+    String dbType = (ms == null || ms == "") ? "mysql" : ms; // default dbType to mysql if management_system is missing
+    DatabaseInfo databaseInfo = new DatabaseInfo(host, user, password);
+    databaseInfo.setDbName(dbName);
+    TestDBManager databaseManager = null;
+    MysqlDatabaseManager mysqlDatabaseManager;
+
+
+    switch (dbType) {
+      case "mysql":
+        mysqlDatabaseManager = new MysqlDatabaseManager(databaseInfo);
+        databaseManager = new TestMysqlDatabaseManager(mysqlDatabaseManager);
+        break;
+      case "mariadb":
+        mysqlDatabaseManager = new MariaDbAppDatabaseManager(databaseInfo);
+        databaseManager = new TestMysqlDatabaseManager(mysqlDatabaseManager);
+        break;
+      case "aurora":
+        mysqlDatabaseManager = new AuroraAppDatabaseManager(databaseInfo);
+        databaseManager = new TestMysqlDatabaseManager(mysqlDatabaseManager);
+        break;
+      case "cb":
+        databaseManager = new TestCBDatabaseManager(databaseInfo);
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid database type: " + dbType);
     }
+    return databaseManager;
+  }
 }
