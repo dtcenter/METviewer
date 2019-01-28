e94495e7a753f7bbc8196064fc050aaf196b4929
TatianaBurek <tatiana@ucar.edu>
2019-01-28 13:05:41 -0700

    added methods to check strings to avoid "Path Manipulation" Fortify category

--- java/edu/ucar/metviewer/MVUtil.java
+++ java/edu/ucar/metviewer/MVUtil.java
@@ -2310,7 +2310,7 @@ public class MVUtil {
   }
 
   public static String domSourceToString(final Document document) {
-    String result="";
+    String result = "";
     TransformerFactory transformerFactory = TransformerFactory.newInstance();
     transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
 
@@ -2343,5 +2343,54 @@ public class MVUtil {
     }
   }
 
+  public static String cleanString(String aString) {
+    if (aString == null) {
+      return null;
+    }
+    StringBuilder cleanString = new StringBuilder();
+    for (int i = 0; i < aString.length(); ++i) {
+      cleanString.append( cleanChar(aString.charAt(i)));
+    }
+    return cleanString.toString();
+  }
+
+  private static char cleanChar(char aChar) {
+
+    // 0 - 9
+    for (int i = 48; i < 58; ++i) {
+      if (aChar == i) {
+        return (char) i;
+      }
+    }
+
+    // 'A' - 'Z'
+    for (int i = 65; i < 91; ++i) {
+      if (aChar == i) {
+        return (char) i;
+      }
+    }
+
+    // 'a' - 'z'
+    for (int i = 97; i < 123; ++i) {
+      if (aChar == i) {
+        return (char) i;
+      }
+    }
+
+    // other valid characters
+    switch (aChar) {
+      case '/':
+        return '/';
+      case '.':
+        return '.';
+      case '-':
+        return '-';
+      case '_':
+        return '_';
+      case ' ':
+        return ' ';
+    }
+    return '%';
+  }
 
 }
