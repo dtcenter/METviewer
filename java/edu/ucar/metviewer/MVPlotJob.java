2fb4d5d8b81ac3fb1e7087987f821f34c1fbb5b3
TatianaBurek <tatiana@ucar.edu>
2019-05-08 10:44:53 -0600

    validate sql for the whole word and not part of it

--- java/edu/ucar/metviewer/MVPlotJob.java
+++ java/edu/ucar/metviewer/MVPlotJob.java
@@ -2025,8 +2025,8 @@ public class MVPlotJob {
   }
 
   private void validateSQL(String str) throws DatabaseException {
-    if (str.toLowerCase().contains("and") && !str.toLowerCase().contains("between")) {
-      throw new DatabaseException("String " + str + " includes SQL unsafe word AND");
+    if (str.toLowerCase().contains(" and ") && !str.toLowerCase().contains(" between ")) {
+      throw new DatabaseException("String " + str + " includes SQL unsafe word AND or BETWEEN");
     }
     for (String noSQL : DatabaseManager.SQL_INJECTION_WORDS) {
       if (str.contains(noSQL.toLowerCase()) || str.contains(noSQL.toUpperCase())) {
