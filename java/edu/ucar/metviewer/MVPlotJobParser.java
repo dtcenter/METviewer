596fa1197f99ef0dbcc3ddbcaae7d5c7547b3f7b
TatianaBurek <tatiana@ucar.edu>
2021-01-05 11:47:11 -0700

    add eqbound parameters #233

--- java/edu/ucar/metviewer/MVPlotJobParser.java
+++ java/edu/ucar/metviewer/MVPlotJobParser.java
@@ -209,6 +209,12 @@ public final class MVPlotJobParser {
               MVPlotJob.class.getDeclaredMethod("setRelyEventHist", String.class));
       formatToStrValues
               .put("ci_alpha", MVPlotJob.class.getDeclaredMethod("setCIAlpha", String.class));
+
+      formatToStrValues
+              .put("eqbound_low", MVPlotJob.class.getDeclaredMethod("setEqboundLow", String.class));
+      formatToStrValues
+              .put("eqbound_high", MVPlotJob.class.getDeclaredMethod("setEqboundHigh", String.class));
+
       formatToStrValues
               .put("ensss_pts", MVPlotJob.class.getDeclaredMethod("setEnsSsPts", String.class));
       formatToStrValues.put("ensss_pts_disp",
@@ -1043,7 +1049,9 @@ public final class MVPlotJobParser {
                     "<reverse_y>" + job.getReverseY() + "</reverse_y>" +
                     "<reverse_x>" + job.getReverseX() + "</reverse_x>" +
                     "<add_contour_overlay>" + job.getAddContourOverlay() + "</add_contour_overlay>" +
-                    "<plot_stat>" + job.getPlotStat() + "</plot_stat>");
+                    "<plot_stat>" + job.getPlotStat() + "</plot_stat>" +
+                    "<eqbound_low>" + job.getEqboundLow() + "</eqbound_low>" +
+                    "<eqbound_high>" + job.getEqboundHigh() + "</eqbound_high>");
     if (job.getContourIntervals() != null) {
       xmlStr.append("<contour_intervals>" + job.getContourIntervals() + "</contour_intervals>");
     }
