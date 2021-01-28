5d561d70507751d5455925de5bae2d3369a4eba7
TatianaBurek <tatiana@ucar.edu>
2021-01-28 11:27:06 -0700

    update jars

--- bin/mv_test.sh
+++ bin/mv_test.sh
@@ -74,7 +74,7 @@ while getopts "t:m:d:u:p:P:k:h:j:cnls:e:a:r?" o; do
             PYTHON_ENV="-Dpython.env=${OPTARG}"
             ;;
         a)
-            METCALCPY_HOME="-Dmetplotpy.env=${OPTARG}"
+            METCALCPY_HOME="-Dmetcalcpy.env=${OPTARG}"
             ;;
         r)
             METPLOTPY_HOME="-Dmetplotpy.env=${OPTARG}"
@@ -139,29 +139,29 @@ fi
 
 
 # construct the classpath for MVLoad
-CLASSPATH=${MV_HOME}/lib/log4j-1.2.15.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/mariadb-java-client-2.4.0.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/xercesImpl.jar
+CLASSPATH=${MV_HOME}/lib/xercesImpl.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/xml-apis.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/tomcat-jdbc-8.5.2.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/juli-6.0.53.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-api-1.7.5.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-log4j12-1.7.5.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/guava-14.0.1.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/junit-4.11.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/hamcrest-core-1.3.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-io-2.4.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-io-2.8.0.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/mockito-all-1.9.5.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/servlet-api.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-fileupload-1.3.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-api-2.10.0.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-core-2.10.0.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/log4j-iostreams-2.10.0.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/servlet-api-4.0.1.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-fileupload-1.4.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/dist/lib/metviewer_all.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/j2html-0.7.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-core-2.8.5.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-databind-2.8.5.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/commons-lang3-3.5.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/j2html-1.4.0.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-core-2.12.0.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/jackson-databind-2.12.0.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/commons-lang3-3.11.jar
+
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/mariadb-java-client-2.7.1.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/tomcat-jdbc-8.5.61.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-api-2.14.0.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-core-2.14.0.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-iostreams-2.14.0.jar
 
 
 
