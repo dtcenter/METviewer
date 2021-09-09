b826a339e3e404b8c6fad6f5bf14ba1dcdf1444c
TatianaBurek <tatiana@ucar.edu>
2021-09-09 11:21:34 -0600

    added SSIDX STAT line type #327

--- sql/mv_mysql.sql
+++ sql/mv_mysql.sql
@@ -1692,6 +1692,39 @@ CREATE TABLE line_data_vcnt
 ) ENGINE = MyISAM
   CHARACTER SET = latin1;
 
+
+
+DROP TABLE IF EXISTS line_data_ssidx;
+CREATE TABLE line_data_ssidx
+(
+    stat_header_id   INT UNSIGNED NOT NULL,
+    data_file_id     INT UNSIGNED NOT NULL,
+    line_num         INT UNSIGNED,
+    fcst_lead        INT,
+    fcst_valid_beg   DATETIME,
+    fcst_valid_end   DATETIME,
+    fcst_init_beg    DATETIME,
+    obs_lead         INT UNSIGNED,
+    obs_valid_beg    DATETIME,
+    obs_valid_end    DATETIME,
+    alpha            DOUBLE,
+    fcst_model       VARCHAR(40),
+    ref_model        VARCHAR(40),
+    n_init           INT UNSIGNED,
+    n_term           INT UNSIGNED,
+    v_vld            INT UNSIGNED,
+    ss_index         DOUBLE DEFAULT -9999,
+
+    CONSTRAINT line_data_ssidx_data_file_id_pk
+        FOREIGN KEY (data_file_id)
+            REFERENCES data_file (data_file_id),
+    CONSTRAINT line_data_ssidx_stat_header_id_pk
+        FOREIGN KEY (stat_header_id)
+            REFERENCES stat_header (stat_header_id),
+    INDEX stat_header_id_idx (stat_header_id)
+) ENGINE = MyISAM
+  CHARACTER SET = latin1;
+
 -- mode_header represents a line in a mode file and contains the header information for
 --   that line.  The line-dependent information is stored in specific tables for each line 
 --   type, each of which point at the line they are associated with, via the mode_header_id 
