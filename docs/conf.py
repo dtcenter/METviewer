0926ab310653524bb8dcd34f0d0da1bcedfa40c6
lisagoodrich <33230218+lisagoodrich@users.noreply.github.com>
2023-03-06 10:45:12 -0700

    adding sphinx_design for dropdown menus

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,8 @@ copyright = f'{release_year}, {author}'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx']
+# Adding 'sphinx_design' to use drop-down menus in release_notes. 
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','sphinx_design']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
