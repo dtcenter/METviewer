198eb2d07cd67d14e999ba974853f14410566fee
bikegeek <minnawin@ucar.edu>
2025-01-23 15:30:38 -0700

    update for beta1

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@ project = 'METviewer'
 author = 'UCAR/NCAR'
 
 author_list = "Jensen, T., I. Srivastava, M. Win-Gildenmeister, L. Goodrich, J. Prestopnik, L. Blank, T. Fowler, and T. Burek"
-version = "3.1.0-beta1-dev"
+version = "6.1.0-beta1"
 verinfo = version
 release = f'{version}'
-release_year = '2024'
-release_date = f'{release_year}-12-18'
+release_year = '2025'
+release_date = f'{release_year}-01-23'
 copyright = f'{release_year}, {author}'
 
 
