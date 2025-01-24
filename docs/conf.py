4082f26e2fb2b2e836efb448ec537ddb1737ae0c
MWin <3753118+bikegeek@users.noreply.github.com>
2025-01-23 17:58:46 -0700

    Update conf.py
    update for beta2 dev cycle

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = 'METviewer'
 author = 'UCAR/NCAR'
 
 author_list = "Jensen, T., I. Srivastava, M. Win-Gildenmeister, L. Goodrich, J. Prestopnik, L. Blank, T. Fowler, and T. Burek"
-version = "6.1.0-beta1"
+version = "6.1.0-beta2-dev"
 verinfo = version
 release = f'{version}'
 release_year = '2025'
