51616c6674c6dc82a0caf952081117d0b266b234
John Halley Gotway <johnhg@ucar.edu>
2025-07-15 13:58:19 -0600

    Per #586, port over recent changes from main_v6.0.

--- .github/jobs/build_docker_image.sh
+++ .github/jobs/build_docker_image.sh
@@ -14,10 +14,19 @@ fi
 
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build.log
 
+# Get METplus Analysis tool versions
+METDATAIO_VERSION=$(${GITHUB_WORKSPACE}/develop/metplus/component_versions.py -i METviewer -v "${SOURCE_BRANCH}" -o METdataio)
+METCALCPY_VERSION=$(${GITHUB_WORKSPACE}/develop/metplus/component_versions.py -i METviewer -v "${SOURCE_BRANCH}" -o METcalcpy)
+METPLOTPY_VERSION=$(${GITHUB_WORKSPACE}/develop/metplus/component_versions.py -i METviewer -v "${SOURCE_BRANCH}" -o METplotpy)
+
 time_command docker build -t ${DOCKERHUB_TAG} \
+    --build-arg METVIEWER_GIT_NAME=${SOURCE_BRANCH} \
     --build-arg SOURCE_BRANCH \
     --build-arg MET_BASE_REPO \
     --build-arg MET_BASE_TAG \
+    --build-arg METDATAIO_VERSION=${METDATAIO_VERSION} \
+    --build-arg METCALCPY_VERSION=${METCALCPY_VERSION} \
+    --build-arg METPLOTPY_VERSION=${METPLOTPY_VERSION} \
     -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
 if [ $? != 0 ]; then
   cat ${CMD_LOGFILE}
