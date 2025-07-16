eaa296f3625d2b40b04817d588b2d7a84230bfe5
John Halley Gotway <johnhg@ucar.edu>
2025-07-16 12:45:40 -0600

    Per #586, update the formatting of the time_command log messages and refine the release-docker-images.yaml verisons.

--- .github/jobs/bash_functions.sh
+++ .github/jobs/bash_functions.sh
@@ -2,24 +2,26 @@
 
 # utility function to run command get log the time it took to run
 # if CMD_LOGFILE is set, send output to that file and unset var
+# ::group:: and ::endgroup:: create collapsible log groups on GitHub Actions
 function time_command {
   local start_seconds=$SECONDS
-  echo "RUNNING: $*"
+  echo "::group::RUNNING: $*"
 
   local error
   # pipe output to log file if set
   if [ "x$CMD_LOGFILE" == "x" ]; then
-      "$@"
-      error=$?
+    "$@"
+    error=$?
   else
-      echo "Logging to ${CMD_LOGFILE}"
-      "$@" &>> $CMD_LOGFILE
-      error=$?
-      unset CMD_LOGFILE
+    echo "Logging to ${CMD_LOGFILE}"
+    "$@" &>> $CMD_LOGFILE
+    error=$?
+    unset CMD_LOGFILE
   fi
 
   local duration=$(( SECONDS - start_seconds ))
   echo "TIMING: Command took `printf '%02d' $(($duration / 60))`:`printf '%02d' $(($duration % 60))` (MM:SS): '$*'"
+  echo "::endgroup::"
   if [ ${error} -ne 0 ]; then
     echo "ERROR: '$*' exited with status = ${error}"
   fi
