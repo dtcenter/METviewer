fba7bcc301177d220bd1f835c72074ec1e8011eb
bikegeek <minnawin@ucar.edu>
2025-08-07 09:49:12 -0600

    Update versions for guava and snakeyaml, comment out juli.jar

--- bin/mv_compare.sh
+++ bin/mv_compare.sh
@@ -70,10 +70,10 @@ fi
 # construct the classpath for MVLoad
 CLASSPATH=${MV_HOME}/lib/xercesImpl.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/xml-apis.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/juli-6.0.53.jar
+#CLASSPATH=$CLASSPATH:${MV_HOME}/lib/juli-6.0.53.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-api-1.7.5.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/slf4j-log4j12-1.7.5.jar
-CLASSPATH=$CLASSPATH:${MV_HOME}/lib/guava-14.0.1.jar
+CLASSPATH=$CLASSPATH:${MV_HOME}/lib/guava-33.4.8.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/junit-4.11.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/hamcrest-core-1.3.jar
 CLASSPATH=$CLASSPATH:${MV_HOME}/lib/commons-io-2.8.0.jar
@@ -91,7 +91,7 @@ CLASSPATH=$CLASSPATH:$MV_HOME/lib/tomcat-jdbc-8.5.61.jar
 CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-api-2.17.1.jar
 CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-core-2.17.1.jar
 CLASSPATH=$CLASSPATH:$MV_HOME/lib/log4j-iostreams-2.17.0.jar
-CLASSPATH=$CLASSPATH:$MV_HOME/lib/snakeyaml-1.27.jar
+CLASSPATH=$CLASSPATH:$MV_HOME/lib/snakeyaml-2.0.jar
 
 echo "Running allRestRunner"
 #/Users/pierce/test_data all
