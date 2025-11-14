333796c8f435c19f2a62e839fd7f72523fd6cd0b
MWin <3753118+bikegeek@users.noreply.github.com>
2025-11-14 06:32:31 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'METviewer'
 author = 'UCAR/NCAR'
 author_list = "Jensen, T., I. Srivastava, M. Win-Gildenmeister, L. Goodrich, J. Prestopnik, L. Blank, T. Fowler, and T. Burek"
-version = "7.0.0-beta-dev"
+version = "7.0.0-beta1-dev"
 verinfo = version
 release = f'{version}'
 release_year = '2025'
