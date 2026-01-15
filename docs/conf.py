f82ab654ccf63f2a90f414a9192cd5559fa15ef1
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-01-15 16:09:05 -0700

    fix version since we will create v6.2.1 bugfix release

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'METviewer'
 author = 'UCAR/NCAR'
 author_list = "Jensen, T., I. Srivastava, M. Win-Gildenmeister, L. Goodrich, J. Prestopnik, L. Blank, T. Fowler, and T. Burek"
-version = "6.2.0"
+version = "6.2.1"
 verinfo = version
 release = f'{version}'
 release_year = '2025'
