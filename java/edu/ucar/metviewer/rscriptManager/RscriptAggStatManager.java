dc080fc2aa01cf0b5dc58f411e22bb834a966692
bikegeek <minnawin@ucar.edu>
2026-05-12 17:25:30 -0600

    Remove env used to set the chrome env var.  Plotly/kaleido/chrome is no longer in use in the METplotpy plotting.

--- java/edu/ucar/metviewer/rscriptManager/RscriptAggStatManager.java
+++ java/edu/ucar/metviewer/rscriptManager/RscriptAggStatManager.java
@@ -94,8 +94,8 @@ public class RscriptAggStatManager extends RscriptStatManager {
           mvResponse = MVUtil.runRscript(mvBatch.getPython(),
                   mvBatch.getMetCalcpyHome() + "/metcalcpy/agg_stat_event_equalize.py",
                   new String[]{eeInfo},
-                  env);
-
+                  new String[]{"PYTHONPATH=" + mvBatch.getMetCalcpyHome()});
+         
 
         }else {
           String tmplFileName = "agg_stat_event_equalize.info_tmpl";
@@ -300,8 +300,7 @@ public class RscriptAggStatManager extends RscriptStatManager {
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        Map<String, String> env = new HashMap<>();
-        env.put("PYTHONPATH", mvBatch.getMetCalcpyHome());
+
         if(job.isModeJob() || job.isMtdJob()){
           mvBatch.print(mvBatch.getPython()
                   + "\n"
@@ -312,7 +311,7 @@ public class RscriptAggStatManager extends RscriptStatManager {
           mvResponse = MVUtil.runRscript(mvBatch.getPython(),
                   mvBatch.getMetCalcpyHome() + rScriptFile,
                   new String[]{aggInfo},
-                  env);
+                  new String[]{"PYTHONPATH=" + mvBatch.getMetCalcpyHome()});
         }else {
 
           mvBatch.print(mvBatch.getPython()
@@ -324,7 +323,7 @@ public class RscriptAggStatManager extends RscriptStatManager {
           mvResponse = MVUtil.runRscript(mvBatch.getPython(),
                   mvBatch.getMetCalcpyHome() + rScriptFile,
                   new String[]{aggInfo},
-                  env);
+                  new String[]{"PYTHONPATH=" + mvBatch.getMetCalcpyHome()});
         }
         stopWatch.stop();
         if (mvResponse.getInfoMessage() != null) {
