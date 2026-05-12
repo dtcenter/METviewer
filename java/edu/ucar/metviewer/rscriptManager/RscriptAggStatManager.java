5d3a7be4d01dbdabd01f04fe6e940bd73b9c871e
bikegeek <minnawin@ucar.edu>
2026-05-12 17:37:42 -0600

    forgot to remove one more use of env

--- java/edu/ucar/metviewer/rscriptManager/RscriptAggStatManager.java
+++ java/edu/ucar/metviewer/rscriptManager/RscriptAggStatManager.java
@@ -88,8 +88,6 @@ public class RscriptAggStatManager extends RscriptStatManager {
                   + mvBatch.getMetCalcpyHome() + "/metcalcpy/agg_stat_event_equalize.py"
                   + " "
                   + eeInfo);
-          Map<String, String> env = new HashMap<>();
-          env.put("PYTHONPATH", mvBatch.getMetCalcpyHome());
           mvResponse = MVUtil.runRscript(mvBatch.getPython(),
                   mvBatch.getMetCalcpyHome() + "/metcalcpy/agg_stat_event_equalize.py",
                   new String[]{eeInfo},
